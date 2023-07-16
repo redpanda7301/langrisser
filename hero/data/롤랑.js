94c3ce58f4579a9ab9617bd5bce37a4ddaf628b8
redpanda7301 <redpanda7301@gmail.com>
2023-07-17 00:13:58 +0900

    롤랑 작업 완료

---
+++ hero/data/롤랑.js
@@ -0,0 +1,604 @@
+﻿var data = [
+{
+  "Skins": [
+    {
+      "ImageUrl": "롤랑/1.webp",
+      "Desc": "기본"
+    },
+    {
+      "ImageUrl": "롤랑/2.webp",
+      "Desc": "빛의 메아리"
+    }
+  ],
+  "Name": "롤랑",
+  "CV": "KENN",
+  "ChName": "罗兰",
+  "EngName": "Roland",
+  "JapName": "",
+  "Rank": [
+    0
+  ],
+  "SP": false,
+  "Groups": [
+    0,
+    1,
+    7
+  ],
+  "HP": 25,
+  "SPHP": 0,
+  "Attk": 35,
+  "SPAttk": 0,
+  "Def": 20,
+  "SPDef": 0,
+  "MagicDef": 20,
+  "SPMagicDef": 0,
+  "HowToGet": "영웅 소환",
+  "Fetters": [
+    {
+      "Condition": "롤랑 30레벨 도달"
+    },
+    {
+      "Condition": "롤랑를 빛의 성재로 전직"
+    },
+    {
+      "Condition": "란디우스의 도움을 받아 운명의 문5 클리어"
+    },
+    {
+      "Condition": "리스틸의 도움을 받아 운명의 문5 클리어"
+    }
+  ],
+  "HeartFetter": "",
+  "Talent": [
+    {
+      "Name": "성스러운 빛의 검",
+      "Icon": "Gift_Roland.webp",
+      "Abilities": [
+        {
+          "Desc": "1성"
+        },
+        {
+          "Desc": "2성"
+        },
+        {
+          "Desc": "지력 <font color=\"red\">+10</font>%<br/>스킬 사용 후 행동 종료 시 자신 부대 생명 <font color=\"red\">10</font>% <font color=\"green\">회복</font><br/>전투 시작 시 영웅 병종에 따라 다른 형태 획득<br/><img src=\"../img/troop/Icon_Occupation_Monk.webp\" width=\"20px\">신관 : <font color=\"blue\">납도</font>' 획득<br/><font color=\"blue\">납도</font> : 지력의 <font color=\"red\">10</font>% 수치를 방어에 더하고<br/>선공으로 전투 진입 시 전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font><br/><img src=\"../img/troop/Icon_Occupation_Magician.webp\" width=\"20px\">마법사 '<font color=\"blue\">눈부심</font>' 획득<br/><font color=\"blue\">눈부심</font> : 스킬 사용 시 <font color=\"blue\">부대 사거리</font> <font color=\"red\">+2</font><br/>선공으로 적에게 피해를 입힌 후 적에게 <font color=\"blue\">무작위 디버프</font> <font color=\"red\">2</font>개 부여<br/><font color=\"blue\">납도</font> 및 <font color=\"blue\">눈부심</font>은 면역 및 해제 불가"
+        },
+        {
+          "Desc": "지력 <font color=\"red\">+13</font>%<br/>스킬 사용 후 행동 종료 시 자신 부대 생명 <font color=\"red\">13</font>% <font color=\"green\">회복</font><br/>전투 시작 시 영웅 병종에 따라 다른 형태 획득<br/><img src=\"../img/troop/Icon_Occupation_Monk.webp\" width=\"20px\">신관 : <font color=\"blue\">납도</font>' 획득<br/><font color=\"blue\">납도</font> : 지력의 <font color=\"red\">10</font>% 수치를 방어에 더하고<br/>선공으로 전투 진입 시 전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font><br/><img src=\"../img/troop/Icon_Occupation_Magician.webp\" width=\"20px\">마법사 '<font color=\"blue\">눈부심</font>' 획득<br/><font color=\"blue\">눈부심</font> : 스킬 사용 시 <font color=\"blue\">부대 사거리</font> <font color=\"red\">+2</font><br/>선공으로 적에게 피해를 입힌 후 적에게 <font color=\"blue\">무작위 디버프</font> <font color=\"red\">2</font>개 부여<br/><font color=\"blue\">납도</font> 및 <font color=\"blue\">눈부심</font>은 면역 및 해제 불가"
+        },
+        {
+          "Desc": "지력 <font color=\"red\">+16</font>%<br/>스킬 사용 후 행동 종료 시 자신 부대 생명 <font color=\"red\">16</font>% <font color=\"green\">회복</font><br/>전투 시작 시 영웅 병종에 따라 다른 형태 획득<br/><img src=\"../img/troop/Icon_Occupation_Monk.webp\" width=\"20px\">신관 : <font color=\"blue\">납도</font>' 획득<br/><font color=\"blue\">납도</font> : 지력의 <font color=\"red\">10</font>% 수치를 방어에 더하고<br/>선공으로 전투 진입 시 전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font><br/><img src=\"../img/troop/Icon_Occupation_Magician.webp\" width=\"20px\">마법사 '<font color=\"blue\">눈부심</font>' 획득<br/><font color=\"blue\">눈부심</font> : 스킬 사용 시 <font color=\"blue\">부대 사거리</font> <font color=\"red\">+2</font><br/>선공으로 적에게 피해를 입힌 후 적에게 <font color=\"blue\">무작위 디버프</font> <font color=\"red\">2</font>개 부여<br/><font color=\"blue\">납도</font> 및 <font color=\"blue\">눈부심</font>은 면역 및 해제 불가"
+        },
+        {
+          "Desc": "지력 <font color=\"red\">+20</font>%<br/>스킬 사용 후 행동 종료 시 자신 부대 생명 <font color=\"red\">20</font>% <font color=\"green\">회복</font><br/>전투 시작 시 영웅 병종에 따라 다른 형태 획득<br/><img src=\"../img/troop/Icon_Occupation_Monk.webp\" width=\"20px\">신관 : <font color=\"blue\">납도</font>' 획득<br/><font color=\"blue\">납도</font> : 지력의 <font color=\"red\">10</font>% 수치를 방어에 더하고<br/>선공으로 전투 진입 시 전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font><br/><img src=\"../img/troop/Icon_Occupation_Magician.webp\" width=\"20px\">마법사 '<font color=\"blue\">눈부심</font>' 획득<br/><font color=\"blue\">눈부심</font> : 스킬 사용 시 <font color=\"blue\">부대 사거리</font> <font color=\"red\">+2</font><br/>선공으로 적에게 피해를 입힌 후 적에게 <font color=\"blue\">무작위 디버프</font> <font color=\"red\">2</font>개 부여<br/><font color=\"blue\">납도</font> 및 <font color=\"blue\">눈부심</font>은 면역 및 해제 불가"
+        }
+      ]
+    }
+  ],
+  "SPTalent": null,
+  "Second_Awaken": null,
+  "Equip": null,
+  "Soldiers": [
+    4,
+    35,
+    17,
+    43,
+    86,
+    3,
+    93,
+    45
+  ],
+  "Jobs": [
+    {
+      "Name": "빛의 성재",
+      "Icon": "Icon_Occupation_Monk.webp",
+      "SD": [
+        {
+          "ImageUrl": "롤랑/1/1.webp"
+        },
+        {
+          "ImageUrl": "롤랑/2/1.webp"
+        }
+      ],
+      "AttkRange": 1,
+      "Move": 3,
+      "MoveType": "Walk",
+      "HP": 4299,
+      "HPRate": 78,
+      "Attk": 256,
+      "AttkRate": 41,
+      "Int": 577,
+      "IntRate": 87,
+      "Def": 325,
+      "DefRate": 73,
+      "MagicDef": 291,
+      "MagicDefRate": 68,
+      "Skill": 149,
+      "SkillRate": 47,
+      "HeartFetterBouns1": "받는 범위 피해 <font color=\"red\">-10</font>%",
+      "HeartFetterBouns2": "<font color=\"blue\">디버프</font>를 보유한 적과 전투시 <br/>주는 피해량 <font color=\"red\">+10</font>%"
+    },
+    {
+      "Name": "허미트",
+      "Icon": "Icon_Occupation_Magician.webp",
+      "SD": [
+        {
+          "ImageUrl": "롤랑/1/2.webp"
+        },
+        {
+          "ImageUrl": "롤랑/2/1.webp"
+        }
+      ],
+      "AttkRange": 2,
+      "Move": 3,
+      "MoveType": "Walk",
+      "HP": 3964,
+      "HPRate": 74,
+      "Attk": 283,
+      "AttkRate": 44,
+      "Int": 542,
+      "IntRate": 84,
+      "Def": 273,
+      "DefRate": 67,
+      "MagicDef": 325,
+      "MagicDefRate": 82,
+      "Skill": 99,
+      "SkillRate": 31,
+      "HeartFetterBouns1": "<font color=\"blue\">디버프</font>를 보유한 적에게 공격받아 전투 진입시 <br/>받는 피해량 <font color=\"red\">-10</font>%",
+      "HeartFetterBouns2": "원거리 피격으로 전투 진입시<br/>주는 피해량 <font color=\"red\">+10</font>%"
+    }
+  ],
+  "Materials": {
+    "HeartFetter": [
+    ],
+    "Fetter": [
+    ],
+    "Awaken": [
+    ]
+  },
+  "ClassUp": [
+    {
+      "ClassTitle": "빛의 검사 : ",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "성스러운 빛의 사도 : ",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "빛의 성재 : 생명+150, 공격+15, 방어+10",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T4",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "서모너 : 생명+80, 방어+5",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "허미트 : 생명+150, 방어+10, 마방+10",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T4",
+          "Materials": [
+          ]
+        }
+      ]
+    }
+  ],
+  "SPOpenConditions": null,
+  "Skills": [
+    // {
+    //   "Name": "성스러운 빛의 검",
+    //   "Cost": "-",
+    //   "Entries": [
+    //     {
+    //       "Label": "유형",
+    //       "Value": "고유기"
+    //     }
+    //   ],
+    //   "IconImageUrl":"../img/ability/Gift_Ronland.webp",
+    //   "Desc": "전투 시작 시 영웅 병종에 따라 다른 형태 획득.",
+    //   "SubSkills": [
+    //     {
+    //       "Name": "<img src=\"../img/troop/Icon_Occupation_Monk.webp\" width=\"20px\">신관",
+    //       "Entries": null,
+    //       "IconImageUrl": "../img/skills/Roland/Gift_Roland2.webp",
+    //       "Desc": "'<font color=\"blue\">납도</font>' 획득<br/><font color=\"blue\">납도</font> : 지력의 <font color=\"red\">10</font>% 수치를 방어에 더하고<br/>선공으로 전투 진입 시 전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font><br/><font color=\"blue\">납도</font>는 면역 및 해제불가"
+    //     },
+    //     {
+    //       "Name": "<img src=\"../img/troop/Icon_Occupation_Magician.webp\" width=\"20px\">마법사",
+    //       "Entries": null,
+    //       "IconImageUrl" : "../img/skills/Roland/Gift_Roland2.webp",
+    //       "Desc": "'<font color=\"blue\">눈부심</font>' 획득<br/><font color=\"blue\">눈부심</font> : 스킬 사용 시 <font color=\"blue\">부대 사거리</font> <font color=\"red\">+2</font><br/>선공으로 적에게 피해를 입힌 후 적에게 <font color=\"blue\">무작위 디버프</font> <font color=\"red\">2</font>개 부여<br/><font color=\"blue\>눈부심</font>은 면역 및 해제 불가"
+    //     },
+    //   ],
+    //   "Animation": null
+    // },
+    {
+      "Name": "징벌",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "마법 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "1턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "2칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Skill_ValkCrash.webp",
+      "Desc": "단일 적 부대를 공격해 <font color=\"red\">1.5배</font>의 피해를 준다.<br/>전투 후 준 피해량의 <font color=\"red\">20%</font>만큼 생명을 <font color=\"green\">회복</font>한다.<br/>근접병사를 휴대할 경우 병사도 같이 공격한다.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "주형",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "액티브"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "2턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "자신"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Roland/1.webp",
+      "Desc": "다른 형태로 바꾸며 자신의 공격 및 지력 <font color=\"red\">+20</font>%<br/>지속 <font color=\"red\">2</font>턴<br/>(전장에 '프레시아'라는 아군이 있을 경우, 지속 시간 <font color=\"red\">4</font>턴으로 변경, 해당 효과를 함께 부여)<br/>사용 후 <font color=\"red\">2</font>칸 <font color=\"blue\">재행동</font>",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "찬란한 빛의 검",
+      "Cost": "-",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "액티브"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "-"
+        },
+        {
+          "Label": "사거리",
+          "Value": "-"
+        },
+        {
+          "Label": "범위",
+          "Value": "-"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Roland/2.webp",
+      "Desc": "현재 형태에 따라 다른 효과. 형태 변환 후 해당 스킬 쿨 초기화",
+      "SubSkills": [
+        {
+          "Name": "납도 - 불태우는 빛",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "마법피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "2칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Roland/2_1.webp",
+          "Desc": "단일 개체 <font color=\"red\">1.5</font>배 피해<br/>전투 전 지력 <font color=\"red\">1</font>배 수치 <font color=\"blue\">고정피해</font><br/><font color=\"blue\">근접전 병사 대동 시 병사도 함께 공격</font>,<font color=\"blue\">근접전 패널티 무효화</font>"
+        },
+        {
+          "Name": "눈부심 - 빛",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "마법피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "6칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "직선"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Roland/2_2.webp",
+          "Desc": "<font color=\"red\">1</font>줄 직선 상 적에게 <font color=\"red\">0.33</font>배 피해 및 <font color=\"blue\">텔레포트</font> 효과<br/>(<font color=\"blue\">명중한 적의 위치를 무작위로 서로 변경</font>)<br/>스킬 사용 전 자신에게 걸린 <font color=\"blue\">디버프</font> <font color=\"red\">2</font>개 <font color=\"blue\">제거</font> 및 <br/>'<font color=\"blue\">공격 지력 저하</font>' '<font color=\"blue\">침묵</font>' <font color=\"blue\">면역</font> 효과 획득<br/>지속 <font color=\"red\">2</font>턴"
+        }
+      ],
+      "Animation": null
+    },
+    {
+      "Name": "신앙",
+      "Cost": "COST 1",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "패시브"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Passive_Belief.webp",
+      "Desc": "공격하여 전투 진입 시 피해량 +<font color=\"red\">15%</font>,<br/>전투 후 가장 생명이 적은 다른 아군 <font color=\"green\">생명 회복</font><br/><font color=\"green\">회복량</font>은 자신 지력의 <font color=\"red\">2</font>배.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "파사",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "마법 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "3칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "3칸"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Skill_BreakEvil.webp",
+      "Desc": "치명타율 <font color=\"red\">+15</font>%<br/>범위 내 모든 적에게 <font color=\"red\">0.3</font>배의 광격 피해를 주고<br/>적이 가진 <font color=\"blue\">버프</font> <font color=\"red\">1</font>개를 제거한다.<br/>'마물'에게 더 큰 피해를 준다.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "홀리 미사일",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "마법 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "6칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "직선"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Skill_SacredRay.webp",
+      "Desc": "<font color=\"red\">1</font>개의 라인에 있는 모든 적군을 공격하여 <font color =\"red\">0.33</font>배 범위 피해를 입힌다.<br/><b>'마물'과 전투 시</b> 더 큰 피해를 입힌다. <font color =\"red\">1</font>개 <font color =\"blue\">버프 제거</font>.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "파사",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "마법 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "3칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "3칸"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Skill_BreakEvil.webp",
+      "Desc": "치명타율 <font color=\"red\">+15</font>%<br/>범위 내 모든 적에게 <font color=\"red\">0.3</font>배의 광격 피해를 주고<br/>적이 가진 <font color=\"blue\">버프</font> <font color=\"red\">1</font>개를 제거한다.<br/>'마물'에게 더 큰 피해를 준다.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "공격 지휘",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "지휘,패시브"
+        }
+      ],
+      "IconImageUrl": "../img/skills/2.webp",
+      "Desc": "주위 <font color=\"red\">2</font>칸 내 모든 아군 공격, 지력 <font color=\"red\">+10</font>%.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "빛의 융합",
+      "Cost": "COST 3",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "액티브"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "-"
+        },
+        {
+          "Label": "사거리",
+          "Value": "-"
+        },
+        {
+          "Label": "범위",
+          "Value": "-"
+        }
+      ],
+      "IconImageUrl": "../img/Super/롤랑.webp",
+      "Desc": "[패시브]근접한 아군을 대신해 모든 공격을 받는다. 위치 이동 면역.<br/>[지휘]주위 2칸 내 모든 아군이 받는 치명타 확률 -30%, 받는 원거리 피해 -15%, 받는 치료 효과 +15%.<br/>[액티브]호위 범위 2칸으로 상승, 지휘 범위 3칸으로 상승,<br/>주위 2칸 범위내 모든 적군의 '재이동 불가', 3턴 지속.",
+      "SubSkills": [
+        {
+          "Name": "납도 - 빛의 응징",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "마법피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "2칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/Super/롤랑_1.webp",
+          "Desc": "단일 개체 <font color=\"red\">1.5</font>배 피해<br/>전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">3</font>개 <font color=\"blue\">제거</font><br/><font color=\"blue\">근접전 병사 대동 시 병사도 함께 공격</font><br/>적이 <font color=\"red\">2</font>개 이상의 <font color=\"blue\">디버프</font>를 보유했을 경우 <font color=\"blue\">선제공격</font><br/>적을 쓰러뜨린 후 자신은 '<font color=\"blue\">적에게 선공으로 피해를 입힌 후, 지력 <font color=\"red\">2</font>배 수치 고정피해 <font color=\"red\">1</font>회 부여</font>' 효과 획득<br/>지속 <font color=\"red\">2</font><br/><font color=\"blue\">근접전 패널티 무효화</font>"
+        },
+        {
+          "Name": "눈부심 - 빛의 경계",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "마법피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "6칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "직선"
+            }
+          ],
+          "IconImageUrl": "../img/Super/롤랑_2.webp",
+          "Desc": "<font color=\"red\">1</font>줄 직선 상 적에게 <font color=\"red\">0.36</font>배 피해 및 <font color=\"blue\">강력한 디버프</font> <font color=\"red\">1</font>개 부여<br/>지속 <font color=\"red\">2</font>행동<br/>적 <font color=\"red\">3</font>명 이상 명중 시, 자신은 '<font color=\"blue\">선공으로 전투 진입 시 피해 <font color=\"red\">+20</font></font>%' 효과 획득<br/>지속 <font color=\"red\">2</font>턴"
+        }
+      ],
+      "Animation": "../img/Super/롤랑_Ani.webp"
+    }
+  ],
+  "Propose": false,
+  "Bio": null,
+  "Performance": null,
+  "Performance2": null,  
+  "Misc": ""
+}];
