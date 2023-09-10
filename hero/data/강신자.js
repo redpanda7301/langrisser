dfe03aab707ca5d9525327ae1cb35b88025c6c8e
redpanda7301 <redpanda7301@gmail.com>
2023-09-10 23:19:15 +0900

    강신자 업로드

---
+++ hero/data/강신자.js
@@ -0,0 +1,502 @@
+﻿var data = [
+{
+  "Skins": [
+    {
+      "ImageUrl": "강신자/1.webp",
+      "Desc": "기본"
+    },
+    {
+      "ImageUrl": "강신자/2.webp",
+      "Desc": "빛의 메아리"
+    }
+  ],
+  "Name": "강신자",
+  "ChName": "神降者",
+  "EngName": "Apotheosis",
+  "JapName": "",
+  "CV": "카키하라 테츠야",
+  "Rank": [
+    0
+  ],
+  "SP": false,
+  "Groups": [    
+    4,
+    7,
+    10
+  ],
+  "HP": 20,
+  "SPHP": 0,
+  "Attk": 40,
+  "SPAttk": 0,
+  "Def": 25,
+  "SPDef": 0,
+  "MagicDef": 15,
+  "SPMagicDef": 0,
+  "HowToGet": "영웅 소환",
+  "Fetters": [
+    {
+      "Condition": "레벨 30 달성"
+    },
+    {
+      "Condition": "강신자를 종말의 신 강림으로 전직"
+    },
+    {
+      "Condition": "강신자를 강림한 재앙으로 전직"
+    },
+    {
+      "Condition": "마크렌의 도움을 받아 운명의문 5 클리어"
+    }
+  ],
+  "HeartFetter": "생명+750, 방어+40, 마방+40",
+  "Talent": [
+    {
+      "Name": "혼돈의 근원",
+      "Icon": "Gift_Apotheosis.webp",
+      "Abilities": [
+        {
+          "Desc": "1성"
+        },
+        {
+          "Desc": "2성"
+        },
+        {
+          "Desc": "<font color=\"blue\">부대 병종 상성 무시</font><br/>공격 <font color=\"red\">+15</font>%<br/>행동 종료시 부대 생명 <font color=\"red\">10</font>% <font color=\"green\">회복</font> 및 공격 스탯의 <font color=\"red\">5</font>% 수치만큼 무작위 스탯 상승<br/>(섕명 제외, 해제 불가)<br/>선공으로 적에게 피해를 입힌 후, <font color=\"red\">50</font>% 확률로 적에게 '<font color=\"purple\">어둠의 마</font>' 혹은 '<font color=\"purple\">마의 선택</font>' 효과 부여<br/><font color=\"purple\">어둠의 마</font> : 제어 불가. 지속 <font color=\"red\">1</font>행동 아레나가 아닌 경우 <font color=\"blue\">주는 피해량</font> <font color=\"red\">-30</font>%로 대체<br/><font color=\"purple\">마의 선택</font> : 영웅 병종 마물로 변경, 지속 <font color=\"red\">1</font>행동"
+        },
+        {
+          "Desc": "<font color=\"blue\">부대 병종 상성 무시</font><br/>공격 <font color=\"red\">+20</font>%<br/>행동 종료시 부대 생명 <font color=\"red\">13</font>% <font color=\"green\">회복</font> 및 공격 스탯의 <font color=\"red\">6</font>% 수치만큼 무작위 스탯 상승<br/>(섕명 제외, 해제 불가)<br/>선공으로 적에게 피해를 입힌 후, <font color=\"red\">60</font>% 확률로 적에게 '<font color=\"purple\">어둠의 마</font>' 혹은 '<font color=\"purple\">마의 선택</font>' 효과 부여<br/><font color=\"purple\">어둠의 마</font> : 제어 불가. 지속 <font color=\"red\">1</font>행동 아레나가 아닌 경우 <font color=\"blue\">주는 피해량</font> <font color=\"red\">-30</font>%로 대체<br/><font color=\"purple\">마의 선택</font> : 영웅 병종 마물로 변경, 지속 <font color=\"red\">1</font>행동"
+        },
+        {
+          "Desc": "<font color=\"blue\">부대 병종 상성 무시</font><br/>공격 <font color=\"red\">+25</font>%<br/>행동 종료시 부대 생명 <font color=\"red\">16</font>% <font color=\"green\">회복</font> 및 공격 스탯의 <font color=\"red\">8</font>% 수치만큼 무작위 스탯 상승<br/>(섕명 제외, 해제 불가)<br/>선공으로 적에게 피해를 입힌 후, <font color=\"red\">80</font>% 확률로 적에게 '<font color=\"purple\">어둠의 마</font>' 혹은 '<font color=\"purple\">마의 선택</font>' 효과 부여<br/><font color=\"purple\">어둠의 마</font> : 제어 불가. 지속 <font color=\"red\">1</font>행동 아레나가 아닌 경우 <font color=\"blue\">주는 피해량</font> <font color=\"red\">-30</font>%로 대체<br/><font color=\"purple\">마의 선택</font> : 영웅 병종 마물로 변경, 지속 <font color=\"red\">1</font>행동"
+        },
+        {
+          "Desc": "<font color=\"blue\">부대 병종 상성 무시</font><br/>공격 <font color=\"red\">+30</font>%<br/>행동 종료시 부대 생명 <font color=\"red\">20</font>% <font color=\"green\">회복</font> 및 공격 스탯의 <font color=\"red\">10</font>% 수치만큼 무작위 스탯 상승<br/>(섕명 제외, 해제 불가)<br/>선공으로 적에게 피해를 입힌 후, <font color=\"red\">100</font>% 확률로 적에게 '<font color=\"purple\">어둠의 마</font>' 혹은 '<font color=\"purple\">마의 선택</font>' 효과 부여<br/><font color=\"purple\">어둠의 마</font> : 제어 불가. 지속 <font color=\"red\">1</font>행동 아레나가 아닌 경우 <font color=\"blue\">주는 피해량</font> <font color=\"red\">-30</font>%로 대체<br/><font color=\"purple\">마의 선택</font> : 영웅 병종 마물로 변경, 지속 <font color=\"red\">1</font>행동"
+        }
+      ]
+    }
+  ],
+  "SPTalent": null,
+  "Second_Awaken": null,
+  "Equip": null,
+  "Soldiers": [
+    64,
+    36,
+    46,
+    56,
+    72,
+    75,
+    97,
+    49,
+    53
+  ],
+  "Jobs": [
+    {
+      "Name": "종말의 신 강림",
+      "Icon": "Icon_Occupation_Monster.webp",
+      "SD": [
+        {
+          "ImageUrl": "강신자/1/1.webp"
+        },
+        {
+          "ImageUrl": "강신자/2/1.webp"
+        }
+      ],
+      "AttkRange": 1,
+      "Move": 5,
+      "MoveType": "Fly",  
+      "HP": 4622,
+      "HPRate": 75,
+      "Attk": 607,
+      "AttkRate": 91,
+      "Int": 249,
+      "IntRate": 37,
+      "Def": 325,
+      "DefRate": 72,
+      "MagicDef": 281,
+      "MagicDefRate": 64,
+      "Skill": 162,
+      "SkillRate": 54,
+      "HeartFetterBouns1": "선공으로 전투 진입 시<br/>부대 받는 피해량 <font color=\"red\">-10</font>%",
+      "HeartFetterBouns2": "근접 피격으로 전투 진입 시<br/>주는 피해량 <font color=\"red\">+10</font>%"
+    },
+    {
+      "Name": "강림한 재앙",
+      "Icon": "Icon_Occupation_Fly.webp",
+      "SD": [
+        {
+          "ImageUrl": "강신자/1/2.webp"
+        },
+        {
+          "ImageUrl": "강신자/2/1.webp"
+        }
+      ],
+      "AttkRange": 1,
+      "Move": 5,
+      "MoveType": "Fly",  
+      "HP": 4322,
+      "HPRate": 71,
+      "Attk": 607,
+      "AttkRate": 91,
+      "Int": 274,
+      "IntRate": 41,
+      "Def": 279,
+      "DefRate": 58,
+      "MagicDef": 315,
+      "MagicDefRate": 81,
+      "Skill": 145,
+      "SkillRate": 46,
+      "HeartFetterBouns1": "피격 범위 피해량 <font color=\"red\">-10</font>%",
+      "HeartFetterBouns2": "<font color=\"blue\">디버프</font>를 보유한 부대와 전투 시<br/>주는 피해량 <font color=\"red\">+10</font>%"
+    }
+  ],
+  "Materials": {
+    "HeartFetter": [
+    ],
+    "Fetter": [
+    ],
+    "Awaken": [
+    ]
+  },
+  "ClassUp": [
+    {
+      "ClassTitle": "심연의 그림자 : ",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "어두운 침식의 근원 :",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "종말의 신 강림 :",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T4",
+          "Materials": [
+
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "하늘을 가리는 찢어진 날개 : ",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        }
+      ]
+    },
+    {
+      "ClassTitle": "강림한 재앙 : ",
+      "Tiers": [
+        {
+          "Name": "T1",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T2",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T3",
+          "Materials": [
+          ]
+        },
+        {
+          "Name": "T4",
+          "Materials": [
+          ]
+        }
+      ]
+    }
+  ],
+  "SPOpenConditions": null,
+  "Skills": [
+    {
+      "Name": "바람 칼날",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "물리 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "2턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "2칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/skills/0.webp",
+      "Desc": "단일 개체에게 <font color=\"Red\">1.5</font>배의 피해<br/>근접전 병사를 이끌고 있다면 병사도 함께 공격한다.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "무기 파괴",
+      "Cost": "COST 1",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "패시브"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Passive_BreakAtk.webp",
+      "Desc": "전투 진입 전 <font color=\"red\">50</font>% 확률로 <br/>적의 <font color=\"blue\">공격, 지력  <font color=\"red\">-20</font></font>%<br/>지속 <font color=\"red\">1</font>행동",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "가속 지원",
+      "Cost": "COST 1",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "패시브"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Passive_SPDBuf1.webp",
+      "Desc": "행동 종료 시 근접한 <font color=\"red\">1</font>개 아군 부대의 이동력 <font color=\"red\">+1</font> 및<br/>'<font color=\"blue\">이동력 저하 면역</font>','<font color=\"blue\">치료 저하 면역</font>'<br/>쿨타임 <font color=\"red\">1</font>행동",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "힘의 외침",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "물리 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "2턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "1칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/skills/36.webp",
+      "Desc": "단일 개체에게 <font color=\"red\">1.3</font>배 피해.<br/>전투 전 상대 <font color=\"blue\">버프</font> <font color=\"red\">2</font>개 제거 및 <font color=\"blue\">공격 및 지력 <font color=\"red\">-20</font></font>%<br/> 지속 <font color=\"red\">1</font>행동",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "어두운 심연의 침식",
+      "Cost": "COST 1",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "패시브, 지휘"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "-"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Apotheosis/1.webp",
+      "Desc": "[지휘] 주위 <font color=\"red\">3</font>칸 범위 안 모든 적에게 행동 종료 시<br/><font color=\"red\">30</font>% 확률로 <font color=\"purple\">마의 선택</font> 효과 부여",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "뇌광",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "물리 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "1칸"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Skill_FlyCrash.webp",
+      "Desc": "단일 개체 <fotn color=\"Red\">1.5</font>배 피해.<br/> 이 스킬로 적을 격파 시 스킬 쿨타임 <font color=\"Red\">-3</font>.",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "바람의 분노",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "물리 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "2턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "자신"
+        },
+        {
+          "Label": "범위",
+          "Value": "1바퀴"
+        }
+      ],
+      "IconImageUrl": "../img/skills/8.webp",
+      "Desc": "주위 <font color=\"red\">1</font>바퀴 내 모든 적군에게 <font color=\"red\">0.3</font>배 범위 피해를 입힌다.<br/>모든 목표에게 <font color=\"blue\">무작위 디버프</font> 부여",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "어두운 파멸의 진동",
+      "Cost": "COST 2",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "물리 피해"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "자신"
+        },
+        {
+          "Label": "범위",
+          "Value": "2칸"
+        }
+      ],
+      "IconImageUrl": "../img/skills/Apotheosis/2.webp",
+      "Desc": "주위 <font color=\"red\">2</font>칸 범위 안 적에게 <font color=\"red\">0.4</font>배 피해<br/>마물 및 신관에게 <font color=\"red\">20</font>% 피해 추가<br/>적 <font color=\"blue\">방어</font> <font color=\"red\">-20</font>%<br/>지속 <font color=\"red\">2</font>행동",
+      "SubSkills": null,
+      "Animation": null
+    },
+    {
+      "Name": "종말의 현신",
+      "Cost": "COST 3",
+      "Entries": [
+        {
+          "Label": "유형",
+          "Value": "변신"
+        },
+        {
+          "Label": "쿨타임",
+          "Value": "3턴"
+        },
+        {
+          "Label": "사거리",
+          "Value": "자신"
+        },
+        {
+          "Label": "범위",
+          "Value": "단일"
+        }
+      ],
+      "IconImageUrl": "../img/Super/강신자.webp",
+      "Desc": "[액티브] '<font color=\"blue\">혼돈</font>' 상태 진입<br/><font color=\"blue\">혼돈</font> : <font color=\"purple\">고유 스킬</font>로 부여하는 <font color=\"blue\">디버프</font> 효과 해제 불가<br/>전투 진입 시 <font color=\"blue\">주는 피해</font> <font color=\"red\">+20</font>%, <font color=\"blue\">받는 피해</font> <font color=\"red\">-20</font>%<br/><font color=\"blue\">어두운 파멸의 진동</font> 사용 시 마물 병종인 모든 부대에게 <font color=\"purple\">어둠의 마</font> 효과 부여<br/>사망 시 <font color=\"blue\">혼돈</font> 상태가 <font color=\"blue\">해제</font>되며 생명 <font color=\"red\">30</font>% 수치로 <font color=\"blue\">부활</font><br/>해당 스킬은 사용 후 <font color=\"blue\">심연의 심판</font>으로 변경<br/> <font color=\"red\">3</font>칸 <font color=\"blue\">재행동</font>",
+      "SubSkills": [
+      {
+        "Name": "심연의 심판",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "3턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/Super/강신자_1.webp",
+        "Desc": "[패시브] 마물 및 신관과 교전 시 영웅 공격 <font color=\"red\">+15</font>%<br/><font color=\"blue\">어두운 심연의 침식</font> <font color=\"blue\">지휘</font> 효과 중 <font color=\"blue\">마의 선택</font> 부여 확률 <font color=\"red\">60</font>%로 상승<br/>[물리피해] 단일 개체 <font color=\"red\">1.6</font>배 피해<br/>전투 전 적의 <font color=\"blue\">버프</font> <font color=\"red\">3</font>개를 <font color=\"blue\">디버프</font>로 <font color=\"blue\">변환</font> 및 <font color=\"red\">50</font>% <font color=\"blue\">치료 반전</font> 효과 부여<br/>(지속 <font color=\"red\">2</font>행동, 해제 불가)<br/>적 격파 시 <font color=\"blue\">해당 스킬</font> 쿨 <font color=\"red\">-2</font> 및 전장의 무작위 적 <font color=\"red\">1</font>명에게 <font color=\"purple\">어둠의 마</font>와 <font color=\"purple\">마의 선택</font> 효과 부여<br/><font color=\"blue\">혼돈</font> 상태가 사라지면 해당 스킬은 <font color=\"blue\">종말의 현신</font>으로 변경"
+      }
+    ],
+      
+      "Animation": "../img/Super/강신자_Ani.webp"
+    }
+  ],
+  "Propose": false,
+  "Bio": null,
+  "Performance": null,
+  "Performance2": null,  
+  "Misc": ""
+}];
