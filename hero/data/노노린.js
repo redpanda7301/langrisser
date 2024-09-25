884c3ab62f5e31d3299b4f7f46b73c18dab946af
Abysswalker-8percent <mms0208@gmail.com>
2024-09-26 04:56:56 +0900

    노노린.js 파일 작성
    노노린.js 파일 작성, 노노린 전용 스킬 아이콘 파일 정보 및 1 2 3 5번 유대 해금 조건 추가 필요

---
+++ hero/data/노노린.js
@@ -0,0 +1,381 @@
+var data = [
+  {
+    "Skins": [
+      {
+        "ImageUrl": "노노린/1.webp",
+        "Desc": "기본"
+      },
+      {
+        "ImageUrl": "노노린/2.webp",
+        "Desc": "빛의 메아리"
+      }
+    ],
+    "Name": "노노린",
+    "ChName": "诺诺琳",
+    "EngName": "Nornorin",
+    "JapName": "",
+    "CV": "세토 아사미",
+    "Rank": [
+      0
+    ],
+    "SP": false,
+    "Groups": [
+      "빛군",
+      "전략",
+      "유성"
+    ],
+    "HP": 5,
+    "SPHP": 0,
+    "Attk": 10,
+    "SPAttk": 0,
+    "Def": 15,
+    "SPDef": 0,
+    "MagicDef": 5,
+    "SPMagicDef": 0,
+    "HowToGet": "영웅 소환",
+    "Fetters": [
+      {
+        "Condition": ""
+      },
+      {
+        "Condition": ""
+      },
+      {
+        "Condition": "프레시아와 함께 운명의문4"
+      },
+      {
+        "Condition": " 전직"
+      }
+    ],
+    "HeartFetter": "생명+750, 방어+50, 마방+50",
+    "Talent": [
+      {
+        "TalentName": "파도를 지배하는 검",
+        "Icon": "Gift_Nornorin.webp",
+        "Abilities": [
+          {
+            "Desc": "1성"
+          },
+          {
+            "Desc": "2성"
+          },
+          {
+            "Desc": "주는 피해량 <font color=\"red\">+15%</font><br/>물에서 행동 종료 시 공격 <font color=\"red\">1</font>배로 생명 회복, 초과 수치는 쉴드로 전환, 해제 불가. (최대 생명 25% 수치를 초과 불가)<br/>행동 종료 시, 주위<font color=\"red\">5</font>칸 범위 안에 <font color=\"purple\">파도 부르기</font> 시전 가능<br/><font color=\"purple\">파도 부르기</font> : <font color=\"red\">3</font>줄 직선 상의 최대 <font color=\"red\">3</font>칸 범위의 적을 시전자로부터 <font color=\"red\">2</font>칸 밀침, 명중한 지형은 <font color=\"red\">2</font>턴 동안 <font color=\"purple\">만조</font> 효과 획득<br/><font color=\"purple\">만조</font> : 자신과 적이 <font color=\"purple\">만조</font> 위에 있을 경우, 해당 지형은 <font color=\"blue\">물</font>로 간주. 이 스킬로 위치 이동 효과를 발동시켰을 경우, 자신이 보유한 버프 지속시간을 소모하지 않으며 재공격 가능 (쿨타임 <font color=\"red\">4</font>행동)"
+          },
+          {
+            "Desc": "주는 피해량 <font color=\"red\">+20%</font><br/>물에서 행동 종료 시 공격 <font color=\"red\">1.5</font>배로 생명 회복, 초과 수치는 쉴드로 전환, 해제 불가. (최대 생명 25% 수치를 초과 불가)<br/>행동 종료 시, 주위<font color=\"red\">5</font>칸 범위 안에 <font color=\"purple\">파도 부르기</font> 시전 가능<br/><font color=\"purple\">파도 부르기</font> : <font color=\"red\">3</font>줄 직선 상의 최대 <font color=\"red\">3</font>칸 범위의 적을 시전자로부터 <font color=\"red\">2</font>칸 밀침, 명중한 지형은 <font color=\"red\">2</font>턴 동안 <font color=\"purple\">만조</font> 효과 획득<br/><font color=\"purple\">만조</font> : 자신과 적이 <font color=\"purple\">만조</font> 위에 있을 경우, 해당 지형은 <font color=\"blue\">물</font>로 간주. 이 스킬로 위치 이동 효과를 발동시켰을 경우, 자신이 보유한 버프 지속시간을 소모하지 않으며 재공격 가능 (쿨타임 <font color=\"red\">3</font>행동)"
+          },
+          {
+            "Desc": "주는 피해량 <font color=\"red\">+25%</font><br/>물에서 행동 종료 시 공격 <font color=\"red\">2</font>배로 생명 회복, 초과 수치는 쉴드로 전환, 해제 불가. (최대 생명 25% 수치를 초과 불가)<br/>행동 종료 시, 주위<font color=\"red\">5</font>칸 범위 안에 <font color=\"purple\">파도 부르기</font> 시전 가능<br/><font color=\"purple\">파도 부르기</font> : <font color=\"red\">3</font>줄 직선 상의 최대 <font color=\"red\">3</font>칸 범위의 적을 시전자로부터 <font color=\"red\">2</font>칸 밀침, 명중한 지형은 <font color=\"red\">2</font>턴 동안 <font color=\"purple\">만조</font> 효과 획득<br/><font color=\"purple\">만조</font> : 자신과 적이 <font color=\"purple\">만조</font> 위에 있을 경우, 해당 지형은 <font color=\"blue\">물</font>로 간주. 이 스킬로 위치 이동 효과를 발동시켰을 경우, 자신이 보유한 버프 지속시간을 소모하지 않으며 재공격 가능 (쿨타임 <font color=\"red\">3</font>행동)"
+          },
+          {
+            "Desc": "주는 피해량 <font color=\"red\">+30%</font><br/>물에서 행동 종료 시 공격 <font color=\"red\">2.5</font>배로 생명 회복, 초과 수치는 쉴드로 전환, 해제 불가. (최대 생명 25% 수치를 초과 불가)<br/>행동 종료 시, 주위<font color=\"red\">5</font>칸 범위 안에 <font color=\"purple\">파도 부르기</font> 시전 가능<br/><font color=\"purple\">파도 부르기</font> : <font color=\"red\">3</font>줄 직선 상의 최대 <font color=\"red\">3</font>칸 범위의 적을 시전자로부터 <font color=\"red\">2</font>칸 밀침, 명중한 지형은 <font color=\"red\">2</font>턴 동안 <font color=\"purple\">만조</font> 효과 획득<br/><font color=\"purple\">만조</font> : 자신과 적이 <font color=\"purple\">만조</font> 위에 있을 경우, 해당 지형은 <font color=\"blue\">물</font>로 간주. 이 스킬로 위치 이동 효과를 발동시켰을 경우, 자신이 보유한 버프 지속시간을 소모하지 않으며 재공격 가능 (쿨타임 <font color=\"red\">2</font>행동)"
+          }
+        ]
+      }
+    ],
+    "SPTalent": null,
+    "Second_Awaken": null,
+    "Equip": null,
+    "Soldiers": [
+      "그레나디어",
+      "중장 창병",
+      "머맨 로드",
+      "발키리",
+      "왕녀 친위대",
+      "빙하의 정령",
+      "군도의 특사",
+      "파도 소환사"
+    ],
+    "Jobs": [
+      {
+        "Name": "암초의 지휘관",
+        "Icon": "보병",
+        "SD": [
+          {
+            "ImageUrl": "노노린/1/1.webp"
+          },
+          {
+            "ImageUrl": "노노린/2/1.webp"
+          }
+        ],
+        "AttkRange": 1,
+        "Move": 3,
+        "MoveType": "수상",
+        "HP": 4086,
+        "HPRate": 62,
+        "Attk": 588,
+        "AttkRate": 90,
+        "Int": 249,
+        "IntRate": 38,
+        "Def": 295,
+        "DefRate": 65,
+        "MagicDef": 241,
+        "MagicDefRate": 53,
+        "Skill": 158,
+        "SkillRate": 45,
+        "HeartFetterBouns1": "<b>자신 부대 보다 생명 백분비가 높은 부대애개 피격 시</b><br/>전투 진입 후 받는 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "<b><font color=\"blue\">물</font>에서</b><br/>주는 물리 피해량 <font color=\"red\">+10%</font>"
+      },
+      {
+        "Name": "파도를 지배하는 별",
+        "Icon": "수병",
+        "SD": [
+          {
+            "ImageUrl": "노노린/1/2.webp"
+          },
+          {
+            "ImageUrl": "노노린/2/1.webp"
+          }
+        ],
+        "AttkRange": 1,
+        "Move": 3,
+        "MoveType": "수상",
+        "HP": 3739,
+        "HPRate": 57,
+        "Attk": 588,
+        "AttkRate": 90,
+        "Int": 226,
+        "IntRate": 34,
+        "Def": 318,
+        "DefRate": 70,
+        "MagicDef": 221,
+        "MagicDefRate": 49,
+        "Skill": 132,
+        "SkillRate": 37,
+        "HeartFetterBouns1": "<b><font color=\"blue\">물</font>에서</b><br/>받는 물리 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "<b>근접 피격으로 전투 진입 시</b><br/>주는 피해량 <font color=\"red\">+10%</font>"
+      }
+    ],
+    "SPOpenConditions": null,
+    "Skills": [
+      {
+        "Name": "파도 퍼트리기",
+        "Cost": "COST 1",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "액티브"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "4턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "자신"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Gourry/1.webp",
+        "Desc": "자신이 <font color=\"blue\">물</font>, <font color=\"blue\">격류</font>, <font color=\"blue\">늪</font>, 및 아군 <font color=\"blue\">만조</font> 지형 통과 시,<br/><font color=\"red\">2</font>칸 이동력 저하 면제 기회 획득.<br/>지속 <font color=\"red\">2</font>행동, <font color=\"red\">2</font>칸 재행동<br/>해당 스킬 사용 후 행동 종료 시 <font color=\"blue\">파도 부르기 사용 불가</font><br/><font color=\"blue\">보유한 버프 지속 시간을 소모하지 않음</font>",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "바람칼날",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "2칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/0.webp",
+        "Desc": "[물리 피해]단일 적 부대를 공격해 1.5배의 피해를 준다.<br/>근접전 병사를 이끌고 있다면 병사도 함께 공격한다.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "일섬",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "1턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_SwordCrash3.webp",
+        "Desc": "단일 개체에게 <font color=\"Red\">1.4</font>배 피해<br/>치명타율 <font color=\"Red\">+20%</font>.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "제압",
+        "Cost": "COST 1",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "패시브"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Passive_AtkBuf1.webp",
+        "Desc": "[패시브]부대의 남은 생명 비율이 상대보다 높을 경우 전투 중 공격 +12%.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "검혼",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "5턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_SwordSoul.webp",
+        "Desc": "단일 개체에게 <font color=\"red\">1.8</font>배 피해<br/>전투 전 적 부대의 버프 <font color=\"red\">5</font>개 <font color=\"blue\">제거</font> 및 적 부대에게 '<font color=\"blue\">치료 방해</font>' 부여<br/> 지속 <font color=\"red\">2<br/>턴<br/>해제 불가",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "파도의 경계",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "3턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "자신"
+          },
+          {
+            "Label": "범위",
+            "Value": "2칸"
+          }
+        ],
+        "IconImageUrl": "../img/skills/0.webp",
+        "Desc": "자신 주위 <font color=\"red\">2</font>칸 이내 적에게 <font color=\"red\">0.36</font>배 피해 및 적에게 <font color=\"blue\">재행동 불가</font> 효과 부여, 지속 <font color=\"red\">2</font>행동. 명중한 지형에는 <font color=\"red\">2</font>턴간 <font color=\"blue\">만조</font> 효과 부여",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "휩쓸기",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "3칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "직선"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Polyal/2.webp",
+        "Desc": "<font color=\"red\">3</font>줄 직선상 모든 적에게 <font color=\"red\">0.25</font>배 피해 및 <font color=\"blue\">위치 이동</font> (<font color=\"red\">2</font>칸 밀침)",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "물의 창",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_WaterCrash.webp",
+        "Desc": "[물리 피해]단일 적 부대를 공격해 1.5배의 피해를 준다.<br/>물 속에 있을 경우 2배의 피해를 준다.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "파도베기",
+        "Cost": "COST 3",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리 피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "3턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/Super/노노린.webp",
+        "Desc": "[패시브] <font color=\"blue\">물</font>에 있는 적과 교전 시 적 방어 <font color=\"red\">20%</font> 무시 및 피해를 입힌 후 적에게 <font color=\"blue\">방어구 무효화 (최대 생명 제외)</font> 효과 부여, 지속 <font color=\"red\">2</font>행동.<br/>상대가 논플레이어 영웅일 경우, 해당 효과는 <font color=\"blue\">방어력</font> <font color=\"red\">-25%</font>로 대체<br/>[물리 피해]단일 개체 <font color=\"red\">1.6</font>배 피해<br/>전투 전 <font color=\"blue\">지휘</font>효과 획득<br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">3</font>칸 범위 안 <font color=\"blue\">물</font>에 위치한 적의 생명을 제외한 올스탯 <font color=\"red\">-10%</font>, <font color=\"blue\">전투 시 적보다 늦게 공격</font>, 지속 <font color=\"red\">2</font>행동, 해제 불가<br/>자신 주위 <font color=\"red\">1</font>바퀴 안 적에게 <font color=\"red\">0.1</font>배 범위 피해, <font color=\"blue\">물</font>에 있을 경우 해당 효과의 피해와 범위가 <font color=\"red\">2</font>배",
+        "SubSkills": null,
+        "Animation": "../img/Super/노노린_Ani.webp"
+      }
+    ],
+    "Propose": false,
+    "Bio": null,
+    "Performance": null,
+    "Performance2": null,
+    "Misc": ""
+  }
+]
