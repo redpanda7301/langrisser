e8dc494db677f0e7f61a47602790fca17bfcc856
Abysswalker-8percent <mms0208@gmail.com>
2025-01-06 02:55:35 +0900

    250106 시트 작업
    이리스 페이지 오탈자 수정, 카스미 페이지 생성/작업, 마리로즈 페이지 생성

---
+++ hero/data/카스미.js
@@ -0,0 +1,327 @@
+var data = [
+    {
+      "Skins": [
+        {
+          "ImageUrl": "카스미/1.webp",
+          "Desc": "기본"
+        },
+        {
+          "ImageUrl": "카스미/2.webp",
+          "Desc": "빛의 메아리"
+        }
+      ],
+      "Name": "카스미",
+      "ChName": "霞",
+      "EngName": "Kasumi",
+      "JapName": "",
+      "CV": "쿠와시마 호우코",
+      "Rank": [
+        0
+      ],
+      "SP": false,
+      "Groups": [
+        "주인공",
+        "유성",
+        "시공"
+      ],
+      "HP": 25,
+      "SPHP": 0,
+      "Attk": 35,
+      "SPAttk": 0,
+      "Def": 20,
+      "SPDef": 0,
+      "MagicDef": 20,
+      "SPMagicDef": 0,
+      "HowToGet": "한정 콜라보 소환",
+      "Fetters": [
+        {
+          "Condition": "레벨 30달성"
+        },
+        {
+          "Condition": "카스미를 출전시켜 시공의 균열 3-1[정예] 클리어"
+        },
+        {
+          "Condition": "아레나 1승"
+        },
+        {
+          "Condition": "목숨을 빼앗는 천신 전직"
+        }
+      ],
+      "HeartFetter": "",
+      "Talent": [
+        {
+          "TalentName": "무환천신류",
+          "Icon": "Gift_Temp.webp",
+          "Abilities": [
+            {
+              "Desc": "1성"
+            },
+            {
+              "Desc": "2성"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">10%</font><br/><font color=\"blue\">부대 근접전 패널티 면역</font><br/>피해를 입힌 후 적에게 <font color=\"blue\">경직</font> 효과 부여<br/><font color=\"blue\">경직</font> : <font color=\"blue\">호위 불가</font>, <font color=\"blue\">선제 공격 불가</font> (지속 <font color=\"red\">2</font> 턴, 해제 불가)<br/>선공으로 전투 진입 시, 적이 <font color=\"blue\">경직</font> 효과를 보유했을 경우<br/><font color=\"blue\">반격 불가</font> 및 전투 후 적 <font color=\"blue\">기절</font> (지속 <font color=\"red\">1</font>턴)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">15%</font><br/><font color=\"blue\">부대 근접전 패널티 면역</font><br/>피해를 입힌 후 적에게 <font color=\"blue\">경직</font> 효과 부여<br/><font color=\"blue\">경직</font> : <font color=\"blue\">호위 불가</font>, <font color=\"blue\">선제 공격 불가</font> (지속 <font color=\"red\">2</font> 턴, 해제 불가)<br/>선공으로 전투 진입 시, 적이 <font color=\"blue\">경직</font> 효과를 보유했을 경우<br/><font color=\"blue\">반격 불가</font> 및 전투 후 적 <font color=\"blue\">기절</font> (지속 <font color=\"red\">1</font>턴)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">20%</font><br/><font color=\"blue\">부대 근접전 패널티 면역</font><br/>피해를 입힌 후 적에게 <font color=\"blue\">경직</font> 효과 부여<br/><font color=\"blue\">경직</font> : <font color=\"blue\">호위 불가</font>, <font color=\"blue\">선제 공격 불가</font> (지속 <font color=\"red\">2</font> 턴, 해제 불가)<br/>선공으로 전투 진입 시, 적이 <font color=\"blue\">경직</font> 효과를 보유했을 경우<br/><font color=\"blue\">반격 불가</font> 및 전투 후 적 <font color=\"blue\">기절</font> (지속 <font color=\"red\">1</font>턴)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">25%</font><br/><font color=\"blue\">부대 근접전 패널티 면역</font><br/>피해를 입힌 후 적에게 <font color=\"blue\">경직</font> 효과 부여<br/><font color=\"blue\">경직</font> : <font color=\"blue\">호위 불가</font>, <font color=\"blue\">선제 공격 불가</font> (지속 <font color=\"red\">2</font> 턴, 해제 불가)<br/>선공으로 전투 진입 시, 적이 <font color=\"blue\">경직</font> 효과를 보유했을 경우<br/><font color=\"blue\">반격 불가</font> 및 전투 후 적 <font color=\"blue\">기절</font> (지속 <font color=\"red\">1</font>턴)"
+            }
+          ]
+        }
+      ],
+      "SPTalent": null,
+      "Second_Awaken": null,
+      "Equip": null,
+      "Soldiers": [
+        "미스트 댄서",
+        "하이엘프",
+        "엘리시움 암살자",
+        "사무라이",
+        "검은 깃털의 가시",
+        "갑판 포병",
+        "황야 답사대"
+      ],
+      "Jobs": [
+        {
+          "Name": "목숨을 빼앗는 천신",
+          "Icon": "보병",
+          "SD": [
+            {
+              "ImageUrl": "카스미/1/1.webp"
+            },
+            {
+              "ImageUrl": "카스미/2/1.webp"
+            }
+          ],
+          "AttkRange": 1,
+          "Move": 3,
+          "MoveType": "보행",
+          "HP": 4201,
+          "HPRate": 64,
+          "Attk": 560,
+          "AttkRate": 86,
+          "Int": 244,
+          "IntRate": 37,
+          "Def": 315,
+          "DefRate": 70,
+          "MagicDef": 280,
+          "MagicDefRate": 62,
+          "Skill": 252,
+          "SkillRate": 72,
+          "HeartFetterBouns1": "피격으로 전투 진입 시<br/>받는 물리 피해량 <font color=\"red\">-10%</font>",
+          "HeartFetterBouns2": "선공으로 전투 진입 시<br/>주는 피해량 <font color=\"red\">-10%</font>"
+        }
+      ],
+      "SPOpenConditions": null,
+      "Skills": [
+        {
+            "Name": "그림자 습격",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "2칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/6.webp",
+            "Desc": "[물리 피해] 호위를 무시하고 단일 적 부대를 공격해 1.3배의 피해를 준다.",
+            "SubSkills": null,
+            "Animation": null
+          },
+        {
+          "Name": "봉신보",
+          "Cost": "COST 1",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "액티브"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "3칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Kasumi/Temp1.webp",
+          "Desc": "액티브. 직선 방향의 빈 곳 혹은 부대에게 사용, <font color=\"blue\">위치 이동</font> 효과 부여 : 자신을 목표 위치로 당긴 후 <font color=\"red\">2</font>칸 <font color=\"blue\">재행동</font><br/>해당 효과는 면역 불가<br/>위치 이동 시 부대 이동 방식은 <font color=\"blue\">비행</font>으로 간주<br/>해당 스킬의 재행동은 보유한 버프 지속 시간이 감소하지 않음",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+            "Name": "암살",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "2칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_BladeCrash2.webp",
+            "Desc": "[물리 피해]호위를 무시하고 단일 적 부대를 공격해 1.2배의 피해를 주고<br/>치명타율 +20%.",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "약탈",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "2칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_BladeCrash3.webp",
+            "Desc": "<font color=\"blue\">호위 무시</font> 및 단일 개체 <font color=\"red\">1.1</font>배의 피해<br/>전투 후 적 버프 <font color=\"Red\">3</font>개 <font color=\"blue\">강탈</font>",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "기습",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "패시브"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Passive_Ass1.webp",
+            "Desc": "[패시브]전투 중 치명타가 발생한 경우<br/>전투 후 적에게 [고정 피해]를 준다.(피해량은 영웅 공격의 2배)",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "트릭스터",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "패시브"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_TrickStep.webp",
+            "Desc": "[패시브]치명타 확률+10%.<br/>전투 중 적 부대를 격퇴할 시 3칸 재이동 가능.",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "천신장",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Kasumi/Temp1.webp",
+            "Desc": "단일 개체 <font color=\"red\">1.4</font>배의 피해 및 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+20%</font> 효과 부여 (지속 <font color=\"red\">1</font>턴)<br/>적이 <font color=\"blue\">경직</font>보유 시,<br/>전투 후 <font color=\"blue\">방어력</font> <font color=\"Red\">-30%</font> 효과 부여. (지속 <font color=\"red\">2</font>턴)",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "야전",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "패시브"
+              }
+            ],
+            "IconImageUrl": "../img/skills/10.webp",
+            "Desc": "[패시브]이동 시 통과할 수 있는 모든 지형을 '평지'로 취급한다.<br/>위치한 지형에 방어 상승 효과가 있을 경우 공격도 똑같이 상승한다.",
+            "SubSkills": null,
+            "Animation": null
+          },
+        {
+          "Name": "앵화천무",
+          "Cost": "COST 3",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "물리피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "4턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "1칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/Super/카스미.webp",
+          "Desc": "[패시브] <font color=\"blue\">경직</font> 효과를 보유한 적과 교전 시, 받는 피해 <font color=\"red\">-30%</font> 및 공격 <font color=\"red\">+30%</font><br/>[물리피해] 단일 개체 <font color=\"red\">1.6</font>배 피해<br/>적이 <font color=\"blue\">경직</font>보유 시, 해당 전투에서 <font color=\"red\">2</font>회 공격<br/>전투 후 자신 주위 <font color=\"red\">3</font>칸 범위 안에 적이 있을 경우 <font color=\"blue\">재행동</font>, 반대일 경우 해당 스킬 쿨 <font color=\"red\">-3</font>턴<br/>해당 효과는 쿨타임 <font color=\"red\">2</font>턴",
+          "SubSkills": null,
+          "Animation": "../img/Super/카스미_Ani.webp"
+        }
+      ],
+      "Propose": false,
+      "Bio": null,
+      "Performance": null,
+      "Performance2": null,
+      "Misc": ""
+    }
+  ]
