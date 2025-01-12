2ed2a2d20772385b0273bd7d2fa386d69359ff7f
Abysswalker-8percent <mms0208@gmail.com>
2025-01-13 03:17:44 +0900

    250113 시트 작업
    마리 로즈 페이지 작업, 뇨텐구 페이지 작업

---
+++ hero/data/뇨텐구.js
@@ -0,0 +1,338 @@
+var data = [
+    {
+      "Skins": [
+        {
+          "ImageUrl": "뇨텐구/1.webp",
+          "Desc": "기본"
+        },
+        {
+          "ImageUrl": "뇨텐구/2.webp",
+          "Desc": "빛의 메아리"
+        }
+      ],
+      "Name": "뇨텐구",
+      "ChName": "女天狗",
+      "EngName": "Nyotengu",
+      "JapName": "",
+      "CV": "사토 아케미",
+      "Rank": [
+        0
+      ],
+      "SP": false,
+      "Groups": [
+        "공주",
+        "인외",
+        "시공"
+      ],
+      "HP": 20,
+      "SPHP": 0,
+      "Attk": 40,
+      "SPAttk": 0,
+      "Def": 15,
+      "SPDef": 0,
+      "MagicDef": 25,
+      "SPMagicDef": 0,
+      "HowToGet": "한정 콜라보 소환",
+      "Fetters": [
+        {
+          "Condition": "레벨 30달성"
+        },
+        {
+          "Condition": "뇨텐구를 출전시켜 시공의 균열 3-1[정예] 클리어"
+        },
+        {
+          "Condition": "아레나 1승"
+        },
+        {
+          "Condition": "요란의 텐구 공주 전직"
+        }
+      ],
+      "HeartFetter": "",
+      "Talent": [
+        {
+          "TalentName": "비천의 춤",
+          "Icon": "Gift_Temp.webp",
+          "Abilities": [
+            {
+              "Desc": "1성"
+            },
+            {
+              "Desc": "2성"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">10%</font><br/>방어 지형에서 행동 종료 시, 혹은 해당 턴에서 스킬을 사용한 경우, 자신의 부대 생명 <font color=\"red\">15%</font> 회복 및 <font color=\"blue\">비천의 춤</font> 효과 획득<br/><font color=\"blue\">비천의 춤</font> : <font color=\"blue\">이동력</font> <font color=\"red\">+2</font>, <font color=\"blue\">이동시 적 통과 가능</font>, <font color=\"blue\">받는 피해량</font> <font color=\"red\">-50%</font>, 피격으로 전투 진입해 피해를 입으면 해당 효과 상실 (해당 방식으로 효과 상실 시, 재획득 쿨 <font color=\"red\">1</font>턴)(지속 <font color=\"red\">2</font>턴, <font color=\"blue\">해제 불가</font>)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">15%</font><br/>방어 지형에서 행동 종료 시, 혹은 해당 턴에서 스킬을 사용한 경우, 자신의 부대 생명 <font color=\"red\">20%</font> 회복 및 <font color=\"blue\">비천의 춤</font> 효과 획득<br/><font color=\"blue\">비천의 춤</font> : <font color=\"blue\">이동력</font> <font color=\"red\">+2</font>, <font color=\"blue\">이동시 적 통과 가능</font>, <font color=\"blue\">받는 피해량</font> <font color=\"red\">-50%</font>, 피격으로 전투 진입해 피해를 입으면 해당 효과 상실 (해당 방식으로 효과 상실 시, 재획득 쿨 <font color=\"red\">1</font>턴)(지속 <font color=\"red\">2</font>턴, <font color=\"blue\">해제 불가</font>)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">20%</font><br/>방어 지형에서 행동 종료 시, 혹은 해당 턴에서 스킬을 사용한 경우, 자신의 부대 생명 <font color=\"red\">25%</font> 회복 및 <font color=\"blue\">비천의 춤</font> 효과 획득<br/><font color=\"blue\">비천의 춤</font> : <font color=\"blue\">이동력</font> <font color=\"red\">+2</font>, <font color=\"blue\">이동시 적 통과 가능</font>, <font color=\"blue\">받는 피해량</font> <font color=\"red\">-50%</font>, 피격으로 전투 진입해 피해를 입으면 해당 효과 상실 (해당 방식으로 효과 상실 시, 재획득 쿨 <font color=\"red\">1</font>턴)(지속 <font color=\"red\">2</font>턴, <font color=\"blue\">해제 불가</font>)"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">25%</font><br/>방어 지형에서 행동 종료 시, 혹은 해당 턴에서 스킬을 사용한 경우, 자신의 부대 생명 <font color=\"red\">30%</font> 회복 및 <font color=\"blue\">비천의 춤</font> 효과 획득<br/><font color=\"blue\">비천의 춤</font> : <font color=\"blue\">이동력</font> <font color=\"red\">+2</font>, <font color=\"blue\">이동시 적 통과 가능</font>, <font color=\"blue\">받는 피해량</font> <font color=\"red\">-50%</font>, 피격으로 전투 진입해 피해를 입으면 해당 효과 상실 (해당 방식으로 효과 상실 시, 재획득 쿨 <font color=\"red\">1</font>턴)(지속 <font color=\"red\">2</font>턴, <font color=\"blue\">해제 불가</font>)"
+            }
+          ]
+        }
+      ],
+      "SPTalent": null,
+      "Second_Awaken": null,
+      "Equip": null,
+      "Soldiers": [
+        "로열 그리폰",
+        "중장 기병",
+        "기계 용기사",
+        "드레고니아 나이트",
+        "태양 전투매",
+        "왕 도마뱀 기수"
+      ],
+      "Jobs": [
+        {
+          "Name": "요란의 텐구 공주",
+          "Icon": "비병",
+          "SD": [
+            {
+              "ImageUrl": "뇨텐구/1/1.webp"
+            },
+            {
+              "ImageUrl": "뇨텐구/2/1.webp"
+            }
+          ],
+          "AttkRange": 1,
+          "Move": 5,
+          "MoveType": "비행",
+          "HP": 4406,
+          "HPRate": 67,
+          "Attk": 607,
+          "AttkRate": 93,
+          "Int": 222,
+          "IntRate": 34,
+          "Def": 307,
+          "DefRate": 68,
+          "MagicDef": 329,
+          "MagicDefRate": 73,
+          "Skill": 161,
+          "SkillRate": 46,
+          "HeartFetterBouns1": "생명 백분비가 자신보다 낮은 부대와 교전 시,<br/>전투 진입 후 받는 피해량 <font color=\"red\">-10%</font>",
+          "HeartFetterBouns2": "부대 생명 <font color=\"red\">100%</font>일 경우,<br/>주는 피해량 <font color=\"red\">+10%</font>"
+        }
+      ],
+      "SPOpenConditions": null,
+      "Skills": [
+        {
+            "Name": "주즈쿠마데",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Nyotengu/Temp1.webp",
+            "Desc": "단일 개체 <font color=\"red\">1.5</font>배 피해<br/>전투 후 적에게 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+20%</font> 효과 부여 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">비천의 춤</font> 보유 시 피해가 <font color=\"red\">1.8</font>배로 증가 및 적에게 다음 턴 <font color=\"blue\">기절</font> 효과 부여",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "바람의 기운",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "패시브"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_FollowingWind.webp",
+            "Desc": "[패시브]공격 전 매 이동 1칸당 물리피해증가 5%(최대 25%)",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "태풍",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "액티브"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "4턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "자신"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Nyotengu/Temp1.webp",
+            "Desc": "액티브, 자신에게 <font color=\"blue\">공격력</font> <font color=\"red\">+20%</font> 및 <font color=\"blue\">태풍</font> 효과 부여 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">태풍</font> : 선공으로 피해를 입힌 후 자신 주위 <font color=\"red\">1</font>바퀴 범위 안 적에게 <font color=\"red\">0.1</font>배 피해 및 <font color=\"blue\">텔레포트</font> 효과 부여 (명중한 적 위치 변화)<br/>사용 후 남은 이동력만큼 <font color=\"blue\">재행동</font> 가능<br/>해당 스킬의 재행동은 보유한 버프 지속 시간을 소모하지 않음",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "뇌광",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_FlyCrash.webp",
+            "Desc": "[물리 피해]단일 적 부대를 공격하여 1.5배의 전투 피해를 준다.<br/> 이 스킬로 적을 처치 시 스킬 쿨타임 -3.",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "바람 타기",
+            "Cost": "COST 1",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "패시브"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Passive_FlyControl.webp",
+            "Desc": "[패시브]생명이 50% 이상인 경우 전투 진입시 받는 모든 피해량 -15%.",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "고보시밧타",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "4칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "직선"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Nyotengu/Temp1.webp",
+            "Desc": "1줄 직선 상 적에게 <font color=\"red\">0.4</font>배 피해<br/>공격 시 스킬 사거리 가장 먼 곳 혹은 범위 내 가장 가까운 적으로 <font color=\"blue\">텔레포트</font><br/><font color=\"blue\">위치 이동</font> 효과 부여 (적을 <font color=\"red\">2</font>칸 밀침, 경로 상에 다른 부대나 진입 불가 지형이 있을 경우, 해당 적은 다음 턴 <font color=\"blue\">기절</font>)",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "분뢰",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Skill_FlyCrash2.webp",
+            "Desc": "[물리 피해]단일 적 부대를 공격하여 1.5배의 전투 피해를 준다.<br/>전투 전 [바람 타기]획득:생명이 50% 이상인 경우 전투 진입 시 받는 피해량 -15%, 2턴 지속.",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "바람의 분노",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "자신"
+              },
+              {
+                "Label": "범위",
+                "Value": "1칸"
+              }
+            ],
+            "IconImageUrl": "../img/skills/8.webp",
+            "Desc": "[물리 피해] 주위 1칸 내 모든 적군에게 0.3배 범위 피해를 입힌다.<br/>모든 목표에게 랜덤으로 디버프 시전.",
+            "SubSkills": null,
+            "Animation": null
+          },
+        {
+          "Name": "화신락",
+          "Cost": "COST 3",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "물리피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "1칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/Super/뇨텐구.webp",
+          "Desc": "단일 개체 <font color=\"red\">1.7</font>배 피해<br/>전투 전 적 버프 <font color=\"red\">3</font>개 제거<br/>전투 후 피해량 <font color=\"red\">20%</font> 수치 생명 회복 및 <font color=\"blue\">지휘</font> 효과 획득<br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">2</font>칸 범위 안 모든 적 <font color=\"blue\">이동력</font> <font color=\"red\">-1</font> 및 <font color=\"blue\">호위 불가</font> (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">비천의 춤</font> 보유 시 해당 공격 피해 <font color=\"red\">+20%</font><br/>적을 격파하지 못했을 경우, 자신의 모든 스킬 쿨 <font color=\"red\">-2</font>턴<br/>반대 시 <font color=\"blue\">재이동</font> <font color=\"red\">4</font>칸",
+          "SubSkills": null,
+          "Animation": "../img/Super/뇨텐구_Ani.webp"
+        }
+      ],
+      "Propose": false,
+      "Bio": null,
+      "Performance": null,
+      "Performance2": null,
+      "Misc": ""
+    }
+  ]
---
+++ hero/data/마리로즈.js
@@ -0,0 +1,325 @@
+var data = [
+    {
+      "Skins": [
+        {
+          "ImageUrl": "마리로즈/1.webp",
+          "Desc": "기본"
+        },
+        {
+          "ImageUrl": "마리로즈/2.webp",
+          "Desc": "빛의 메아리"
+        }
+      ],
+      "Name": "마리로즈",
+      "ChName": "玛莉·萝丝",
+      "EngName": "Marie Rose",
+      "JapName": "",
+      "CV": "아이자와 마이",
+      "Rank": [
+        0
+      ],
+      "SP": false,
+      "Groups": [
+        "공주",
+        "유성",
+        "시공"
+      ],
+      "HP": 30,
+      "SPHP": 0,
+      "Attk": 40,
+      "SPAttk": 0,
+      "Def": 20,
+      "SPDef": 0,
+      "MagicDef": 10,
+      "SPMagicDef": 0,
+      "HowToGet": "한정 콜라보 소환",
+      "Fetters": [
+        {
+          "Condition": "레벨 30달성"
+        },
+        {
+          "Condition": "마리로즈를 출전시켜 시공의 균열 3-1[정예] 클리어"
+        },
+        {
+          "Condition": "아레나 1승"
+        },
+        {
+          "Condition": "소악마 메이드 전직"
+        }
+      ],
+      "HeartFetter": "",
+      "Talent": [
+        {
+          "TalentName": "시스테마",
+          "Icon": "Gift_Temp.webp",
+          "Abilities": [
+            {
+              "Desc": "1성"
+            },
+            {
+              "Desc": "2성"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">10%</font><br/>스킬로 공격 후 적에게 <font color=\"blue\">위치 이동</font> 효과 부여 (목표를 <font color=\"red\">3</font>칸 밀치며, 밀쳐지는 경로에 다른 부대나 진입 불가 지형이 있을 경우 다시 <font color=\"blue\">위치 이동</font> 효과가 발동되며 남은 칸수만큼 반대로 튕겨나옴<br/>부대를 위치 이동 시킬때마다 <font color=\"blue\">준비 스텝</font> 획득, 턴 시작 시 제거됨<br/>행동 종료 시, <font color=\"blue\">준비 스텝</font>을 <font color=\"red\">1</font>개 소모해 자신이 보유한 버프 지속시간을 소모하지 않고 추가로 <font color=\"red\">2</font>칸 재행동 가능<br/>해당 재행동 효과는 발동 시킬 때마다 소모하는 준비 스텝의 수가 <font color=\"red\">2</font>배로 되며, 각 턴에 최대 <font color=\"red\">5</font>회 발동"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">15%</font><br/>스킬로 공격 후 적에게 <font color=\"blue\">위치 이동</font> 효과 부여 (목표를 <font color=\"red\">3</font>칸 밀치며, 밀쳐지는 경로에 다른 부대나 진입 불가 지형이 있을 경우 다시 <font color=\"blue\">위치 이동</font> 효과가 발동되며 남은 칸수만큼 반대로 튕겨나옴<br/>부대를 위치 이동 시킬때마다 <font color=\"blue\">준비 스텝</font> 획득, 턴 시작 시 제거됨<br/>행동 종료 시, <font color=\"blue\">준비 스텝</font>을 <font color=\"red\">1</font>개 소모해 자신이 보유한 버프 지속시간을 소모하지 않고 추가로 <font color=\"red\">2</font>칸 재행동 가능<br/>해당 재행동 효과는 발동 시킬 때마다 소모하는 준비 스텝의 수가 <font color=\"red\">2</font>배로 되며, 각 턴에 최대 <font color=\"red\">5</font>회 발동"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">20%</font><br/>스킬로 공격 후 적에게 <font color=\"blue\">위치 이동</font> 효과 부여 (목표를 <font color=\"red\">3</font>칸 밀치며, 밀쳐지는 경로에 다른 부대나 진입 불가 지형이 있을 경우 다시 <font color=\"blue\">위치 이동</font> 효과가 발동되며 남은 칸수만큼 반대로 튕겨나옴<br/>부대를 위치 이동 시킬때마다 <font color=\"blue\">준비 스텝</font> 획득, 턴 시작 시 제거됨<br/>행동 종료 시, <font color=\"blue\">준비 스텝</font>을 <font color=\"red\">1</font>개 소모해 자신이 보유한 버프 지속시간을 소모하지 않고 추가로 <font color=\"red\">3</font>칸 재행동 가능<br/>해당 재행동 효과는 발동 시킬 때마다 소모하는 준비 스텝의 수가 <font color=\"red\">2</font>배로 되며, 각 턴에 최대 <font color=\"red\">5</font>회 발동"
+            },
+            {
+              "Desc": "공격, 방어 <font color=\"red\">25%</font><br/>스킬로 공격 후 적에게 <font color=\"blue\">위치 이동</font> 효과 부여 (목표를 <font color=\"red\">3</font>칸 밀치며, 밀쳐지는 경로에 다른 부대나 진입 불가 지형이 있을 경우 다시 <font color=\"blue\">위치 이동</font> 효과가 발동되며 남은 칸수만큼 반대로 튕겨나옴<br/>부대를 위치 이동 시킬때마다 <font color=\"blue\">준비 스텝</font> 획득, 턴 시작 시 제거됨<br/>행동 종료 시, <font color=\"blue\">준비 스텝</font>을 <font color=\"red\">1</font>개 소모해 자신이 보유한 버프 지속시간을 소모하지 않고 추가로 <font color=\"red\">3</font>칸 재행동 가능<br/>해당 재행동 효과는 발동 시킬 때마다 소모하는 준비 스텝의 수가 <font color=\"red\">2</font>배로 되며, 각 턴에 최대 <font color=\"red\">5</font>회 발동"
+            }
+          ]
+        }
+      ],
+      "SPTalent": null,
+      "Second_Awaken": null,
+      "Equip": null,
+      "Soldiers": [
+        "가면 메이드",
+        "가고일",
+        "송곳니 근위대",
+        "사막용병",
+        "아이언 엔젤"
+      ],
+      "Jobs": [
+        {
+          "Name": "소악마 메이드",
+          "Icon": "보병",
+          "SD": [
+            {
+              "ImageUrl": "마리로즈/1/1.webp"
+            },
+            {
+              "ImageUrl": "마리로즈/2/1.webp"
+            }
+          ],
+          "AttkRange": 1,
+          "Move": 3,
+          "MoveType": "보행",
+          "HP": 4552,
+          "HPRate": 70,
+          "Attk": 590,
+          "AttkRate": 90,
+          "Int": 267,
+          "IntRate": 41,
+          "Def": 343,
+          "DefRate": 76,
+          "MagicDef": 276,
+          "MagicDefRate": 61,
+          "Skill": 175,
+          "SkillRate": 50,
+          "HeartFetterBouns1": "선공으로 전투 진입 시<br/>부대 받는 피해량 <font color=\"red\">-10%</font>",
+          "HeartFetterBouns2": "피격으로 전투 진입 시<br/>주는 피해량 <font color=\"red\">+10%</font>"
+        }
+      ],
+      "SPOpenConditions": null,
+      "Skills": [
+        {
+          "Name": "바보바보바보",
+          "Cost": "COST 1",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "액티브"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "자신"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/skills/MarieRose/Temp1.webp",
+          "Desc": "[패시브] 턴마다 적을 처음 <font color=\"blue\">위치 이동</font> 시켰으나 성공하지 못했을 경우, 행동 종료 시 직선 <font color=\"red\">3</font>칸 범위 안 임의의 장소로 텔레포트 시키고, 해당 텔레포트는 <font color=\"blue\">위치 이동</font> 효과로 간주<br/>액티브. 자신 최대 생명 <font color=\"red\">30%</font> 회복, 주위 <font color=\"red\">4</font>칸 범위 안에 적이 있을 경우 <font color=\"blue\">준비 스텝</font> <font color=\"red\">1</font>개 획득",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+          "Name": "로즈 힙",
+          "Cost": "COST 1",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "물리피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "3턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "직선"
+            },
+            {
+              "Label": "범위",
+              "Value": "4칸"
+            }
+          ],
+          "IconImageUrl": "../img/skills/MarieRose/Temp1.webp",
+          "Desc": "직선 범위 내 적에게 <font color=\"red\">0.1</font>배 피해<br/>공격 시 스킬 사거리 가장 먼 곳까지 텔레포트<br/>적 명중 시 <font color=\"blue\">준비 스텝</font> <font color=\"red\">1</font>개 획득",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+          "Name": "힘의 외침",
+          "Cost": "COST 2",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "물리피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "2턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "1칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/skills/36.webp",
+          "Desc": "[물리 피해] 단일 적군을 공격하여 1.3배 피해를 준다.<br/>전투 전 상대 버프 2개 제거.동시에 상대의 공격 및 지력 -20%, 1턴 지속.",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+          "Name": "불굴",
+          "Cost": "COST 1",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "패시브"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Skill_NerverDie.webp",
+          "Desc": "[패시브]사망 시 부활 및 생명 20%회복. 최대 1회 가능.",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+          "Name": "제압",
+          "Cost": "COST 1",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "패시브"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Passive_AtkBuf1.webp",
+          "Desc": "부대의 남은 생명 비율이 상대보다 높을 경우 전투 중 공격 <font color=\"Red\">+12%</font>.",
+          "SubSkills": null,
+          "Animation": null
+        },
+        {
+          "Name": "공격 위협",
+          "Cost": "COST 2",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "패시브"
+            }
+          ],
+          "IconImageUrl": "../img/skills/Passive_AuraAtkDown.webp",
+          "Desc": "[패시브]행동 종료 시 주위 2칸 내 모든 적 부대 공격, 지력 -15%, 1턴 지속.",
+          "SubSkills": null,
+          "Animation": null
+        },
+          {
+            "Name": "문라이트 스윕",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "자신"
+              },
+              {
+                "Label": "범위",
+                "Value": "1바퀴"
+              }
+            ],
+            "IconImageUrl": "../img/skills/MarieRose/Temp1.webp",
+            "Desc": "주위 <font color=\"red\">1</font>바퀴 범위 내 적에게 <font color=\"red\">0.36</font>배의 피해<br/>공격 후 적의 위치가 바뀌지 않았다면 적의 버프 <font color=\"red\">1</font>개 제거 (<font color=\"blue\">위치 이동 면역</font> 효과가 최우선) 및 <font color=\"blue\">패시브 봉인</font> 부여 (지속 <font color=\"red\">2</font>턴)",
+            "SubSkills": null,
+            "Animation": null
+          },
+          {
+            "Name": "피어나는 장미",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "3턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/MarieRose/Temp1.webp",
+            "Desc": "단일 개체 <font color=\"red\">1.6</font>배의 피해<br/><font color=\"blue\">준비 스텝</font>보유 시, <font color=\"blue\">반격 불가</font><br/>전투 후 적은 다음 턴 <font color=\"blue\">기절</font>",
+            "SubSkills": null,
+            "Animation": null
+          },
+        {
+          "Name": "앵화천무",
+          "Cost": "COST 3",
+          "Entries": [
+            {
+              "Label": "유형",
+              "Value": "물리피해"
+            },
+            {
+              "Label": "쿨타임",
+              "Value": "5턴"
+            },
+            {
+              "Label": "사거리",
+              "Value": "1칸"
+            },
+            {
+              "Label": "범위",
+              "Value": "단일"
+            }
+          ],
+          "IconImageUrl": "../img/Super/마리로즈.webp",
+          "Desc": "[패시브] <font color=\"blue\">위치 이동</font> 효과 발동 시, 이동 경로에 다른 부대나 진입 불가 지형이 있을 경우 자신 공격력 <font color=\"red\">1</font>배 수치 고정 피해 <font color=\"red\">1</font>회 부여<br/>[물리피해] 단일 개체 <font color=\"red\">1.8</font>배 피해<br/>전투 전 적 버프 <font color=\"red\">5</font>개 제거<br/>해당 턴에 고유 스킬 재행동을 발동 시켰었을 경우, 해당 전투는 <font color=\"blue\">선제 공격</font><br/>해당 턴에서 발동시킨 고유 스킬 재행동 횟수마다 해당 스킬 피해 <font color=\"red\">+15%</font> (최대 <font color=\"red\">30%</font>), 전투 후 자신의 모든 스킬 쿨 <font color=\"red\">-1</font>턴 (최대 <font color=\"red\">-2</font>턴)",
+          "SubSkills": null,
+          "Animation": "../img/Super/마리로즈_Ani.webp"
+        }
+      ],
+      "Propose": false,
+      "Bio": null,
+      "Performance": null,
+      "Performance2": null,
+      "Misc": ""
+    }
+  ]
