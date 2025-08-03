72d2f0d62c876807e64695c11bdf6cb9032b5ab9
Abysswalker-8percent <mms0208@gmail.com>
2025-08-04 02:23:46 +0900

    250804 시트 작업
    비리아의 '저주 마법' -> '주문' 으로 표현 수정 (월광연화님 요청사항), 페어리테일 콜라보 영웅 관련 이미지/스킬 아이콘/각성기 애니메이션 추가, 엘자 시트 페이지 개선 및 공개 처리, 마카에라/실나 고백 이미지 업로드

--- hero/data/엘자.js
+++ hero/data/엘자.js
@@ -32,7 +32,7 @@
     "SPDef": 0,
     "MagicDef": 20,
     "SPMagicDef": 0,
-    "HowToGet": "콜라보 이벤트 ",
+    "HowToGet": "영웅 소환 (콜라보)",
     "Fetters": [
       {
         "Condition": "레벨 30 돌파 "
@@ -60,16 +60,16 @@
             "Desc": "2성"
           },
           {
-            "Desc": "공격 <font color=\"red\">+15%</font><br/><font color=\"blue\">부대 이동력</font><font color=\"red\">+1</font></font> 전장에 있는 적의 수가 아군보다 많을 경우<br/> <font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대<font color=\"red\">3</font>배<br/><font color=\"blue\">선공</font>으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(PVP 이외 제한 없음) 쿨타임 (<font color=\"red\">5</font>)<br/>전투 시작 시 '사망 후 생명<font color=\"red\">20%</font>부활, <font color=\"red\">1</font><font color=\"blue\">스택</font>획득, 선공으로 적을 격파할 때마다<br/><font color=\"red\">1</font><font color=\"blue\">스택</font> 추가 획득, 최대 <font color=\"red\">2</font>중첩<br/> <font color=\"blue\">면역 및 해제 불가</font>"
+            "Desc": "공격 <font color=\"red\">+15%</font><br/><font color=\"blue\">부대 이동력</font> <font color=\"red\">+1</font>, 전장에 있는 적의 수가 아군보다 많을 경우<br/><font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/>선공으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(아레나 모드 이외에서는 제한 없음), 쿨타임 <font color=\"red\">5</font>턴<br/>전투 시작 시 '<font color=\"blue\">사망 후 생명<font color=\"red\">20%</font>부활</font>' <font color=\"red\">1</font>스택 획득<br/>선공으로 적을 격파할 때마다 <font color=\"red\">1</font>스택 추가 획득<br/>최대 <font color=\"red\">2</font>스택, <font color=\"blue\">면역 및 해제 불가</font>"
           },
           {
-            "Desc": "공격 <font color=\"red\">+20%</font><br/><font color=\"blue\">부대 이동력</font><font color=\"red\">+1</font> 전장에 있는 적의 수가 아군보다 많을 경우<br/> <font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대<font color=\"red\">3</font>배<br/><font color=\"blue\">선공</font>으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(PVP 이외 제한 없음) 쿨타임 (<font color=\"red\">4</font>)<br/>전투 시작 시 '사망 후 생명<font color=\"red\">20%</font>부활, <font color=\"red\">1</font><font color=\"blue\">스택</font>획득, 선공으로 적을 격파할 때마다<br/><font color=\"red\">1</font><font color=\"blue\">스택</font> 추가 획득, 최대 <font color=\"red\">2</font>중첩<br/> <font color=\"blue\">면역 및 해제 불가</font>"
+            "Desc": "공격 <font color=\"red\">+20%</font><br/><font color=\"blue\">부대 이동력</font> <font color=\"red\">+1</font>, 전장에 있는 적의 수가 아군보다 많을 경우<br/><font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/>선공으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(아레나 모드 이외에서는 제한 없음), 쿨타임 <font color=\"red\">4</font>턴<br/>전투 시작 시 '<font color=\"blue\">사망 후 생명<font color=\"red\">20%</font>부활</font>' <font color=\"red\">1</font>스택 획득<br/>선공으로 적을 격파할 때마다 <font color=\"red\">1</font>스택 추가 획득<br/>최대 <font color=\"red\">2</font>스택, <font color=\"blue\">면역 및 해제 불가</font>"
           },
           {
-            "Desc": "공격 <font color=\"red\">+25%</font><br/><font color=\"blue\">부대 이동력</font><font color=\"red\">+2</font> 전장에 있는 적의 수가 아군보다 많을 경우<br/> <font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대<font color=\"red\">3</font>배<br/><font color=\"blue\">선공</font>으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(PVP 이외 제한 없음) 쿨타임 (<font color=\"red\">3</font>)<br/>전투 시작 시 '사망 후 생명<font color=\"red\">20%</font>부활, <font color=\"red\">1</font><font color=\"blue\">스택</font>획득, 선공으로 적을 격파할 때마다<br/><font color=\"red\">1</font><font color=\"blue\">스택</font> 추가 획득, 최대 <font color=\"red\">2</font>중첩<br/> <font color=\"blue\">면역 및 해제 불가</font>"
+            "Desc": "공격 <font color=\"red\">+25%</font><br/><font color=\"blue\">부대 이동력</font> <font color=\"red\">+2</font>, 전장에 있는 적의 수가 아군보다 많을 경우<br/><font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/>선공으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(아레나 모드 이외에서는 제한 없음), 쿨타임 <font color=\"red\">3</font>턴<br/>전투 시작 시 '<font color=\"blue\">사망 후 생명<font color=\"red\">20%</font>부활</font>' <font color=\"red\">1</font>스택 획득<br/>선공으로 적을 격파할 때마다 <font color=\"red\">1</font>스택 추가 획득<br/>최대 <font color=\"red\">2</font>스택, <font color=\"blue\">면역 및 해제 불가</font>"
           },
           {
-            "Desc": "공격 <font color=\"red\">+30%</font><br/><font color=\"blue\">부대 이동력</font><font color=\"red\">+2</font> 전장에 있는 적의 수가 아군보다 많을 경우<br/> <font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대<font color=\"red\">3</font>배<br/><font color=\"blue\">선공</font>으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(PVP 이외 제한 없음) 쿨타임 (<font color=\"red\">3</font>)<br/>전투 시작 시 '사망 후 생명<font color=\"red\">20%</font>부활, <font color=\"red\">1</font><font color=\"blue\">스택</font>획득, 선공으로 적을 격파할 때마다<br/><font color=\"red\">1</font><font color=\"blue\">스택</font> 추가 획득, 최대 <font color=\"red\">2</font>중첩<br/> <font color=\"blue\">면역 및 해제 불가</font>"
+            "Desc": "공격 <font color=\"red\">+30%</font><br/><font color=\"blue\">부대 이동력</font> <font color=\"red\">+2</font>, 전장에 있는 적의 수가 아군보다 많을 경우<br/><font color=\"red\">1</font>명 당 해당 효과는 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/>선공으로 전투 진입 후, 적의 수가 아군 이상일 때 남은 <font color=\"blue\">이동력</font>만큼 <font color=\"blue\">재행동</font><br/>(아레나 모드 이외에서는 제한 없음), 쿨타임 <font color=\"red\">3</font>턴<br/>전투 시작 시 '<font color=\"blue\">사망 후 생명<font color=\"red\">20%</font>부활</font>' <font color=\"red\">1</font>스택 획득<br/>선공으로 적을 격파할 때마다 <font color=\"red\">1</font>스택 추가 획득<br/>최대 <font color=\"red\">2</font>스택, <font color=\"blue\">면역 및 해제 불가</font>"
           }
         ]
       }
@@ -78,6 +78,8 @@
     "Second_Awaken": null,
     "Equip": null,
     "Soldiers": [
+      "광전사",
+      "용기병",
       "야만용사",
       "듀얼리스트",
       "가면 메이드",
@@ -90,7 +92,7 @@
         "Icon": "보병",
         "SD": [
           {
-            "ImageUrl": "엘자/1/3.webp"
+            "ImageUrl": "엘자/1/1.webp"
           },
           {
             "ImageUrl": "엘자/2/1.webp"
@@ -100,47 +102,20 @@
         "Move": 3,
         "MoveType": "보행",
         "HP": 4985,
-        "HPRate": 74,
+        "HPRate": 76,
         "Attk": 590,
-        "AttkRate": 88,
+        "AttkRate": 90,
         "Int": 242,
         "IntRate": 37,
         "Def": 326,
-        "DefRate": 78,
+        "DefRate": 72,
         "MagicDef": 295,
-        "MagicDefRate": 67,
+        "MagicDefRate": 65,
         "Skill": 144,
-        "SkillRate": 46,
-        "HeartFetterBouns1": "<font color=\"blue\">선공</font>으로 전투 진입 시<br/>부대가 받는 피해량 <font color=\"red\">-10%</font>",
-        "HeartFetterBouns2": "자신보다 생명력이 낮은 부대를<br/> 상대 시 전투 중 주는 피해량 <font color=\"red\">+10%</font>"
+        "SkillRate": 41,
+        "HeartFetterBouns1": "선공으로 전투 진입 시<br/>부대가 받는 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "자신보다 생명력이 낮은 부대를 상대 시<br/>전투 중 주는 피해량 <font color=\"red\">+10%</font>"
       },
-    ],
-    "Materials": {
-      "HeartFetter": null,
-      "Fetter": null,
-      "Awaken": null
-    },
-    "ClassUp": [
-      {
-        "ClassTitle": "용병 :",
-        "Tiers": null
-      },
-      {
-        "ClassTitle": "계승자 :",
-        "Tiers": null
-      },
-      {
-        "ClassTitle": "요정 여왕 :",
-        "Tiers": null
-      },
-      {
-        "ClassTitle": "검사 :",
-        "Tiers": null
-      },
-      {
-        "ClassTitle": "영웅 :",
-        "Tiers": null
-      }
     ],
     "SPOpenConditions": null,
     "Skills": [
@@ -238,22 +213,8 @@
             "SubSkills": null,
             "Animation": null
           },
-          {
-            "Name": "공격 위협",
-            "Cost": "COST 2",
-            "Entries": [
-              {
-                "Label": "유형",
-                "Value": "패시브"
-              }
-            ],
-            "IconImageUrl": "../img/skills/Passive_AuraAtkDown.webp",
-            "Desc": "[패시브]행동 종료 시 주위 <font color=\"red\">2</font>칸 내 모든 적 부대 공격, 지력 <font color=\"red\">-15%</font>, <font color=\"red\">1</font>턴 지속.",
-            "SubSkills": null,
-            "Animation": null
-          },
       {
-        "Name": "무장 변환",
+        "Name": "무장 변환 - 천륜의 갑옷",
         "Cost": "COST 1",
         "Entries": [
           {
@@ -265,15 +226,16 @@
             "Value": "4턴"
           },
            {
-            "Label": "범위",
+            "Label": "사거리",
             "Value": "자신"
           },
           {
-
+            "Label": "범위",
+            "Value": "단일"
           }
         ],
         "IconImageUrl": "../img/skills/Erza/1.webp",
-        "Desc": "<font color=\"blue\">천륜의 갑옷</font> : 변신 액티브. <font color=\"blue\">천륜의 갑옷</font>으로 무장 변환<br/> (야전 효과, 행동 종료 및 선공으로 전투 진입 전 해제 불가능한 '<font color=\"blue\">마법검</font>' 효과 획득)<br/><br/> (선공으로 전투 진입 후 피해를 입힌 후, 목표 주위 <font color=\"red\">2</font>칸 범위 안에 있는 적의 병사 격파,<br/> <font color=\"blue\">마법검</font> <font color=\"red\">1</font>개마다 적 병사 1명 격파, <font color=\"blue\">천륜의 갑옷</font> 효과 상실 시 해당 효과도 <font color=\"blue\">제거</font>,<br/> 위험 지대 안에 있을 경우 추가로 <font color=\"red\">1</font>개 획득, 최대 <font color=\"red\">5</font>개) 지속 <font color=\"red\">3</font>행동 <br/><font color=\"blue\">면역 및 해제 불가</font><br/><br/> 사용 후 남은 <font color=\"blue\">이동력</font><font color=\"red\">+2</font>만큼 <font color=\"blue\">재행동</font>,<br/> 해당 <font color=\"blue\">재행동</font>은 버프 지속 시간을 소모하지 않음",
+        "Desc": "액티브. <font color=\"blue\">천륜의 갑옷</font>으로 무장 변환<br/><font color=\"blue\">천륜의 갑옷</font> : 이동하는 모든 지형을 평지로 간주<br/>행동 종료 및 선공으로 전투 진입 전 해제 불가능한 <font color=\"blue\">마법검</font> 효과 획득<br/>위험 지대 안에 있을 경우 추가로 <font color=\"red\">1</font>개 획득, 최대 <font color=\"red\">5</font>개<br/>지속 <font color=\"red\">3</font>턴<br/><font color=\"blue\">면역 및 해제 불가</font><br/>사용 후 남은 이동력 <font color=\"red\">+2</font>만큼 <font color=\"blue\">재행동</font><br/><font color=\"blue\">해당 재행동은 버프 지속 시간을 소모하지 않음</font><br/><br/><font color=\"blue\">마법검</font> : 선공으로 전투 진입하여 피해를 입힌 후, 목표 주위 <font color=\"red\">2</font>칸 범위 안에 있는 적의 병사 격파<br/><font color=\"blue\">마법검</font> <font color=\"red\">1</font>개마다 적 병사 1명 격파<br/><font color=\"blue\">천륜의 갑옷</font> 효과 상실 시 해당 효과도 <font color=\"blue\">제거</font>",
         "SubSkills": null,
         "Animation": null
       },
@@ -317,7 +279,7 @@
           },
           {
             "Label": "사거리",
-            "Value": "1칸"
+            "Value": "2칸"
           },
           {
             "Label": "범위",
@@ -325,8 +287,32 @@
           }
         ],
         "IconImageUrl": "../img/skills/Erza/2.webp",
-        "Desc": "단일 개체 <font color=\"red\">1.5</font>배 피해, 전투 전 적의 공격 및 지력 <font color=\"red\">-25%</font>, 지속 <font color=\"red\">2</font> 행동.<br/> <font color=\"blue\">마법검</font> 보유 시 전투 전 적에게 영웅 공격 <font color=\"red\">0.5배 </font><font color=\"blue\">고정 피해</font>,<br/> <font color=\"blue\">마법검</font> 갯수당 배율 <font color=\"red\">+0.5배</font>, 근접전 병사 대동 시 함께 공격, <br/>해당 스킬은 <font color=\"blue\">근접전 패널티</font> 영향을 받지 않음",
-        "SubSkills": null,
+        "Desc": "단일 개체 <font color=\"red\">1.5</font>배 피해, 전투 전 적의 공격 및 지력 <font color=\"red\">-25%</font> (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">마법검</font> 보유 시 전투 전 적에게 영웅 공격 <font color=\"red\">0.5</font>배 수치 <font color=\"blue\">고정 피해</font>,<br/><font color=\"blue\">마법검</font> 갯수당 배율 <font color=\"red\">+0.5</font>배<br/>근접전 병사 대동 시 함께 공격<br/>해당 스킬은 <font color=\"blue\">근접전 페널티 영향을 받지 않음</font>",
+        "SubSkills": [
+          {
+            "Name": "홍앵 베기",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "-"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/Erza/3.webp",
+            "Desc": "<font color=\"blue\">각성기를 사용해 '요도 - 홍앵' 효과가 지속 중일 때, 이 스킬로 효과가 변경됨</font><br/>[패시브] 전투 후 피해량의 <font color=\"red\">30%</font> 수치만큼 생명 회복<br/>[물리피해] 단일 개체 <font color=\"red\">1.4</font>배 피해 및 자신의 모든 스킬 쿨타임 <font color=\"red\">-1</font>턴"
+          }
+        ],
         "Animation": null
       },
       {
@@ -350,10 +336,10 @@
             "Value": "단일"
           },
         ],
-        "IconImageUrl": "../img/",
-        "Desc": "[패시브] 아군 사망 시 자신에게 걸린 <font color=\"blue\">디버프</font>  <font color=\"red\">2</font>개 제거, 해당 스킬<font color=\"red\">-5</font><br/>[물리 피해] <font color=\"blue\">무장 변환 갑옷</font>을 포기하고 <font color=\"blue\">요도 홍앵</font> 획득.<br/> <br/>단일 개체 <font color=\"red\">1.7</font>배, 전투 진입 전 적 버프 <font color=\"red\">3</font>개 제거. 전장에 있는 적의 수가<br/> 아군보다 많을 때, 해당 전투에서 적의 <font color=\"blue\">부활 무효화</font> (해당 효과는 아레나에서만 발동) <br/><br/><font color=\"blue\">요도 홍앵</font> : <font color=\"blue\">천륜 - 요란의 검</font>을 <font color=\"blue\">'홍앵 베기'</font>로 전환 후 즉시 사용, 선공으로 전투 진입 시 영웅 방어 <font color=\"red\">-30%</font> <br/>선제 공격 및 적 방어 <font color=\"red\">30%</font> 무시, 전투 전 부대 생명 <font color=\"red\">30%</font> 회복, 적 <font color=\"blue\">버프</font> <font color=\"red\">1</font>개 제거</font><br/> 아군보다 많은 적의 수 <font color=\"red\">1</font>명당 영웅 방어 감소 이외의 효과 수치 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/> <font color=\"blue\">면역 및 해제 불가</font>, 지속 <font color=\"red\">2</font>행동<br/><br/>[패시브] <font color=\"blue\">홍앵 베기</font> : 전투 후 피해량의 <font color=\"red\">30%</font> 수치만큼 생명 회복,<br/> 단일 개체 <font color=\"red\">1.4</font>배 피해 및 자신의 모든 스킬 쿨 <font color=\"red\">-1</font>",
+        "IconImageUrl": "../img/Super/엘자.webp",
+        "Desc": "[패시브] 아군 사망 시 자신에게 걸린 디버프 <font color=\"red\">2</font>개 제거, 해당 스킬 쿨타임 <font color=\"red\">-5</font>턴<br/>[물리피해] <font color=\"blue\">무장 변환 갑옷</font>을 포기하고 <font color=\"blue\">요도 홍앵</font> 획득.<br/>단일 개체 <font color=\"red\">1.7</font>배 피해<br/>전투 진입 전 적 버프 <font color=\"red\">3</font>개 제거<br/>전장에 있는 적의 수가 아군보다 많을 때, 해당 전투에서 적의 <font color=\"blue\">부활 무효화</font> (해당 효과는 아레나 모드에서만 발동)<br/><br/><font color=\"blue\">요도 홍앵</font> : <font color=\"blue\">천륜 - 요란의 검</font>을 <font color=\"blue\">'홍앵 베기'</font>로 전환 후 즉시 사용 가능<br/>선공으로 전투 진입 시 영웅 방어 <font color=\"red\">-30%</font><br/><font color=\"blue\">선제 공격 및 적 방어 <font color=\"red\">30%</font> 무시</font><br/>전투 전 부대 생명 <font color=\"red\">30%</font> 회복<br/>적 버프 <font color=\"red\">1</font>개 제거</font><br/>아군보다 많은 적의 수 <font color=\"red\">1</font>명당 영웅 방어 감소 이외의 효과 수치 <font color=\"red\">1</font>배씩 증가, 최대 <font color=\"red\">3</font>배<br/><font color=\"blue\">면역 및 해제 불가</font><br/>지속 <font color=\"red\">2</font>턴",
         "SubSkills": null,
-        "Animation": "../img/"
+        "Animation": "../img/Super/엘자_Ani.webp"
       }
     ],
     "Propose": false,
