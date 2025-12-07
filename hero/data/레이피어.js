9973027ed2a5adf60bb73fb433b2671ac3da8026
Abysswalker-8percent <mms0208@gmail.com>
2025-12-08 03:08:09 +0900

    251208 시트 작업
    샤리크 페이지 오픈, 레이피어 페이지 작업 및 오픈, 샤리크 페이지에 병사 2종 이미지 표시되지 않는 이슈 수정

--- hero/data/레이피어.js
+++ hero/data/레이피어.js
@@ -41,10 +41,10 @@ var data = [
         "Condition": "레이피어를 출전시켜 시공의 균열 3-1[정예] 클리어"
       },
       {
-        "Condition": "시온의 도움을 받아 오랜 고질병 클리어"
+        "Condition": "시온의 도움을 받아 운명의 문 4 클리어"
       },
       {
-        "Condition": "무관의 황제로 전직"
+        "Condition": "레이피어를 무관의 황제로 전직"
       }
     ],
     "HeartFetter": "생명+750, 방어+60, 마방+30",
@@ -60,16 +60,16 @@ var data = [
             "Desc": "2성"
           },
           {
-            "Desc": "공격, 방어 +<font color=\"red\">10%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 '<font color=\"blue\">결전</font>' 부여 (지속 <font color=\"red\">2</font> 턴, <font color=\"blue\">제거 불가</font>, <font color=\"blue\">면역 불가</font>)<br/> [<font color=\"blue\">결전</font>] : <font color=\"blue\">스킬</font>을 사용하여 공격 시 공격 대상이 [<b>레이피어</b>]가 아니라면 전투 시 <font color=\"blue\">버프</font> <font color=\"red\">5</font>개 제거 (쿨타임 <font color=\"red\">4</font> 턴)<br/>아군이 [<font color=\"blue\">결전</font>]을 보유한 적과 전투에 돌입하면 해당 아군 근처로 [<b>레이피어</b>]가 순간 이동.<br/> 턴 종료 상태라면 <font color=\"blue\">재행동</font> 부여. (매 턴 <font color=\"red\">1</font>번만 발동)<br/> [<font color=\"blue\">결전</font>]을 가진 적과 전투 시 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font> (<font color=\"blue\">부활 불가</font> 는 PVP 아레나 한정으로만 발동)"
+            "Desc": "공격, 방어 <font color=\"red\">+10%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 <font color=\"orange\">정의의 결전</font> 효과 부여 (지속 <font color=\"red\">2</font>행동)<br/><font color=\"orange\">정의의 결전</font> : 스킬을 사용해 선공 시 레벨을 제외한 적이 전투 진입 전 버프 <font color=\"red\">5</font>개 제거 (쿨 <font color=\"red\">4</font>턴, <font color=\"blue\">면역 및 해제 불가</font>)<br/>아군이 <font color=\"orange\">정의의 결전</font> 목표와 전투 진입 시 자신을 해당 아군과 가장 가까운 빈 공간으로 텔레포트<br/>이미 행동 종료했을 경우 <font color=\"blue\">재행동</font> (재행동 효과 쿨 <font color=\"red\">1</font>턴)<br/><font color=\"orange\">정의의 결전</font>을 보유한 적과 전투 시, 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font>, <font color=\"blue\">부활 불가</font> 효과는 아레나에서만 발동"
           },
           {
-            "Desc": "공격, 방어 +<font color=\"red\">15%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 '<font color=\"blue\">결전</font>' 부여 (지속 <font color=\"red\">2</font> 턴, <font color=\"blue\">제거 불가</font>, <font color=\"blue\">면역 불가</font>)<br/> [<font color=\"blue\">결전</font>] : <font color=\"blue\">스킬</font>을 사용하여 공격 시 공격 대상이 [<b>레이피어</b>]가 아니라면 전투 시 <font color=\"blue\">버프</font> <font color=\"red\">5</font>개 제거 (쿨타임 <font color=\"red\">3</font> 턴)<br/>아군이 [<font color=\"blue\">결전</font>]을 보유한 적과 전투에 돌입하면 해당 아군 근처로 [<b>레이피어</b>]가 순간 이동.<br/> 턴 종료 상태라면 <font color=\"blue\">재행동</font> 부여. (매 턴 <font color=\"red\">1</font>번만 발동)<br/> [<font color=\"blue\">결전</font>]을 가진 적과 전투 시 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font> (<font color=\"blue\">부활 불가</font> 는 PVP 아레나 한정으로만 발동)"
+            "Desc": "공격, 방어 <font color=\"red\">+15%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 <font color=\"orange\">정의의 결전</font> 효과 부여 (지속 <font color=\"red\">2</font>행동)<br/><font color=\"orange\">정의의 결전</font> : 스킬을 사용해 선공 시 레벨을 제외한 적이 전투 진입 전 버프 <font color=\"red\">5</font>개 제거 (쿨 <font color=\"red\">3</font>턴, <font color=\"blue\">면역 및 해제 불가</font>)<br/>아군이 <font color=\"orange\">정의의 결전</font> 목표와 전투 진입 시 자신을 해당 아군과 가장 가까운 빈 공간으로 텔레포트<br/>이미 행동 종료했을 경우 <font color=\"blue\">재행동</font> (재행동 효과 쿨 <font color=\"red\">1</font>턴)<br/><font color=\"orange\">정의의 결전</font>을 보유한 적과 전투 시, 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font>, <font color=\"blue\">부활 불가</font> 효과는 아레나에서만 발동"
           },
           {
-            "Desc": "공격, 방어 +<font color=\"red\">20%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 '<font color=\"blue\">결전</font>' 부여 (지속 <font color=\"red\">2</font> 턴, <font color=\"blue\">제거 불가</font>, <font color=\"blue\">면역 불가</font>)<br/> [<font color=\"blue\">결전</font>] : <font color=\"blue\">스킬</font>을 사용하여 공격 시 공격 대상이 [<b>레이피어</b>]가 아니라면 전투 시 <font color=\"blue\">버프</font> <font color=\"red\">5</font>개 제거 (쿨타임 <font color=\"red\">2</font> 턴)<br/>아군이 [<font color=\"blue\">결전</font>]을 보유한 적과 전투에 돌입하면 해당 아군 근처로 [<b>레이피어</b>]가 순간 이동.<br/> 턴 종료 상태라면 <font color=\"blue\">재행동</font> 부여. (매 턴 <font color=\"red\">1</font>번만 발동)<br/> [<font color=\"blue\">결전</font>]을 가진 적과 전투 시 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font> (<font color=\"blue\">부활 불가</font> 는 PVP 아레나 한정으로만 발동)"
+            "Desc": "공격, 방어 <font color=\"red\">+20%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 <font color=\"orange\">정의의 결전</font> 효과 부여 (지속 <font color=\"red\">2</font>행동)<br/><font color=\"orange\">정의의 결전</font> : 스킬을 사용해 선공 시 레벨을 제외한 적이 전투 진입 전 버프 <font color=\"red\">5</font>개 제거 (쿨 <font color=\"red\">2</font>턴, <font color=\"blue\">면역 및 해제 불가</font>)<br/>아군이 <font color=\"orange\">정의의 결전</font> 목표와 전투 진입 시 자신을 해당 아군과 가장 가까운 빈 공간으로 텔레포트<br/>이미 행동 종료했을 경우 <font color=\"blue\">재행동</font> (재행동 효과 쿨 <font color=\"red\">1</font>턴)<br/><font color=\"orange\">정의의 결전</font>을 보유한 적과 전투 시, 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font>, <font color=\"blue\">부활 불가</font> 효과는 아레나에서만 발동"
           },
           {
-            "Desc": "공격, 방어 +<font color=\"red\">25%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 '<font color=\"blue\">결전</font>' 부여 (지속 <font color=\"red\">2</font> 턴, <font color=\"blue\">제거 불가</font>, <font color=\"blue\">면역 불가</font>)<br/> [<font color=\"blue\">결전</font>] : <font color=\"blue\">스킬</font>을 사용하여 공격 시 공격 대상이 [<b>레이피어</b>]가 아니라면 전투 시 <font color=\"blue\">버프</font> <font color=\"red\">5</font>개 제거 (쿨타임 <font color=\"red\">2</font> 턴)<br/>아군이 [<font color=\"blue\">결전</font>]을 보유한 적과 전투에 돌입하면 해당 아군 근처로 [<b>레이피어</b>]가 순간 이동.<br/> 턴 종료 상태라면 <font color=\"blue\">재행동</font> 부여. (매 턴 <font color=\"red\">1</font>번만 발동)<br/> [<font color=\"blue\">결전</font>]을 가진 적과 전투 시 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font> (<font color=\"blue\">부활 불가</font> 는 PVP 아레나 한정으로만 발동)"
+            "Desc": "공격, 방어 <font color=\"red\">+25%</font><br/>행동 종료 시 적 <font color=\"red\">1</font>명을 선택해 <font color=\"orange\">정의의 결전</font> 효과 부여 (지속 <font color=\"red\">2</font>행동)<br/><font color=\"orange\">정의의 결전</font> : 스킬을 사용해 선공 시 레벨을 제외한 적이 전투 진입 전 버프 <font color=\"red\">5</font>개 제거 (쿨 <font color=\"red\">2</font>턴, <font color=\"blue\">면역 및 해제 불가</font>)<br/>아군이 <font color=\"orange\">정의의 결전</font> 목표와 전투 진입 시 자신을 해당 아군과 가장 가까운 빈 공간으로 텔레포트<br/>이미 행동 종료했을 경우 <font color=\"blue\">재행동</font> (재행동 효과 쿨 <font color=\"red\">1</font>턴)<br/><font color=\"orange\">정의의 결전</font>을 보유한 적과 전투 시, 적은 <font color=\"blue\">선제 공격 불가</font> 및 <font color=\"blue\">부활 불가</font>, <font color=\"blue\">부활 불가</font> 효과는 아레나에서만 발동"
           }
         ]
       }
@@ -78,7 +78,11 @@ var data = [
     "Second_Awaken": null,
     "Equip": null,
     "Soldiers": [
-       "송곳니 근위대",
+      "중장 보병",
+      "가면 메이드",
+      "중장 기병",
+      "로열 그리폰",
+      "송곳니 근위대",
       "왕녀 친위대",
       "불꽃 카발리에",
       "암흑 수정 용기병"
@@ -99,19 +103,19 @@ var data = [
         "Move": 3,
         "MoveType": "보행",
         "HP": 4967,
-        "HPRate": 74,
+        "HPRate": 76,
         "Attk": 595,
         "AttkRate": 91,
         "Int": 249,
-        "IntRate": 39,
+        "IntRate": 38,
         "Def": 321,
-        "DefRate": 74,
+        "DefRate": 71,
         "MagicDef": 291,
-        "MagicDefRate": 78,
+        "MagicDefRate": 64,
         "Skill": 115,
-        "SkillRate": 37,
-        "HeartFetterBouns1": "<font color=\"red\">약화 효과</font>를 가진 적에게 공격을 받아 전투에 진입 할 때 <br/>받는 피해량 <font color=\"red\">-10%</font>",
-        "HeartFetterBouns2": "공격하여 전투 진입 시 피해량 +<font color=\"red\">10%</font>"
+        "SkillRate": 41,
+        "HeartFetterBouns1": "<font color=\"red\">디버프</font>를 보유한 부대에게 피격당해 전투 진입 시<br/>받는 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "피격으로 전투 진입 시<br/>주는 피해량 <font color=\"red\">+10%</font>"
       },
       {
         "Name": "광란의 사령관",
@@ -125,22 +129,22 @@ var data = [
           }
         ],
         "AttkRange": 1,
-        "Move": 3,
+        "Move": 5,
         "MoveType": "기마",
         "HP": 4615,
-        "HPRate": 74,
+        "HPRate": 71,
         "Attk": 595,
         "AttkRate": 91,
         "Int": 226,
-        "IntRate": 39,
+        "IntRate": 34,
         "Def": 297,
-        "DefRate": 70,
+        "DefRate": 66,
         "MagicDef": 270,
-        "MagicDefRate": 76,
+        "MagicDefRate": 60,
         "Skill": 121,
-        "SkillRate": 52,
-        "HeartFetterBouns1": "<font color=\"blue\">버프</font>를 <font color=\"red\">7</font>개 이상 보유 시, 전투 진입 후 받는 피해량 -<font color=\"red\">10%</font>",
-        "HeartFetterBouns2": "부대의 생명이  <font color=\"red\">80%</font> 이상일 때 전투 중 피해량  +<font color=\"red\">10%</font>"
+        "SkillRate": 34,
+        "HeartFetterBouns1": "<font color=\"red\">7</font>개 이상의 <font color=\"blue\">버프</font>보유 시<br/>전투 진입 후 받는 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "부대 생명 <font color=\"red\">80%</font> 초과 시<br/>전투 중 주는 피해량 <font color=\"red\">+10%</font>"
       }
     ],
     "SPOpenConditions": null,
@@ -180,60 +184,48 @@ var data = [
             "Value": "패시브"
           },
         ],
-        "IconImageUrl": "../img/skills/1.webp",
-        "Desc": "[패시브] <font color=\"blue\">결전</font>을 보유한 적과 전투 시, 생명을 제외한 모든 스탯 +<font color=\"red\">15%</font><br/>전투 전 자신의 <font color=\"blue\">디버프</font> 제거 및 목표의 해제 가능한 모든 <font color=\"blue\">버프</font> 복제",
+        "IconImageUrl": "../img/skills/Rapier/Temp.webp",
+        "Desc": "[패시브] <font color=\"orange\">정의의 결전</font>을 보유한 적과 전투 시, 생명을 제외한 <font color=\"blue\">모든 스탯</font> <font color=\"red\">+15%</font>, 전투 전 자신의 <font color=\"blue\">디버프 제거 및 목표의 해제 가능한 모든 버프 복제</font>",
         "SubSkills": null,
         "Animation": null
       },
       {
-        "Name": "새끼양 돌격",
-        "Cost": "COST 2",
-        "Entries": [
-          {
-            "Label": "유형",
-            "Value": "물리피해"
+            "Name": "힘의 외침",
+            "Cost": "COST 2",
+            "Entries": [
+              {
+                "Label": "유형",
+                "Value": "물리피해"
+              },
+              {
+                "Label": "쿨타임",
+                "Value": "2턴"
+              },
+              {
+                "Label": "사거리",
+                "Value": "1칸"
+              },
+              {
+                "Label": "범위",
+                "Value": "단일"
+              }
+            ],
+            "IconImageUrl": "../img/skills/36.webp",
+            "Desc": "[물리 피해] 단일 적군을 공격하여 1.3배 피해를 준다.<br/>전투 전 상대 버프 2개 제거.동시에 상대의 공격 및 지력 -20%, 1턴 지속.",
+            "SubSkills": null,
+            "Animation": null
           },
-          {
-            "Label": "쿨타임",
-            "Value": "3턴"
-          },
-          {
-            "Label": "사거리",
-            "Value": "6칸"
-          },
-          {
-            "Label": "범위",
-            "Value": "직선"
-          }
-        ],
-        "IconImageUrl": "../img/skills/Gintoki/1.webp",
-        "Desc": "<font color=\"red\">1</font>줄 직선 상 적에게 <font color=\"red\">0.4</font>배 피해 및 <font color=\"blue\">무기 스킬 무효화</font> 부여<br/>지속 <font color=\"Red\">2</font>행동<br/>NPC 영웅일 경우 해당 효과는 공격,지력 <font color=\"Red\">-25%</font>로 대체 <br/><b>멀어지는 순서대로</b> 피해량 <font color=\"red\">-20%</font> (최대 <font color=\"red\">-60%</font>) 및 <font colro =\"blue\">위치 이동</font> 효과 <br/>(적을 <font color=\"red\">3</font>칸 밀침)",
-        "SubSkills": null,
-        "Animation": null
-      },
       {
-        "Name": "수호의 맹세",
+        "Name": "제압",
         "Cost": "COST 1",
         "Entries": [
           {
             "Label": "유형",
-            "Value": "지원"
-          },
-          {
-            "Label": "쿨타임",
-            "Value": "15턴"
-          },
-          {
-            "Label": "사거리",
-            "Value": "3칸"
-          },
-          {
-            "Label": "범위",
-            "Value": "단일"
+            "Value": "패시브"
           }
         ],
-        "IconImageUrl": "../img/skills/Gintoki/2.webp",
-        "Desc": "소환물이 아닌 다른 아군에게 '<font color =\"blue\">수호의 맹세</font>' 효과 부여 <br/><font color =\"blue\">수호의 맹세</font> : 아군이 <font color =\"red\">3</font>칸 범위 안에 있을 경우 피격 물리 공격 <font color=\"blue\">호위</font> <br/><b><font color =\"blue\">호위</font> 도중 <font color=\"red\">사망</font>할 경우</b> <font color =\"red\">50%</font> 생명 수치로 <font color =\"blue\">부활</font> <br/>최대 <font color =\"red\">1</font>회 발동 <br/><font color =\"blue\">수호의 맹세</font>는 <b>해제 불가, 면역 불가</b> <br/><b><font color =\"blue\">수호의 맹세</font>가 걸린 아군이 <font color=\"red\">사망</font> 시</b><br/> 영구 <font color =\"purple\">백야차</font> 상태 돌입",
+        "IconImageUrl": "../img/skills/Passive_AtkBuf1.webp",
+        "Desc": "부대의 남은 생명 비율이 상대보다 높을 경우 전투 중 공격 <font color=\"Red\">+12%</font>.",
         "SubSkills": null,
         "Animation": null
       },
@@ -264,8 +256,34 @@ var data = [
         "Animation": null
       },
       {
-        "Name": "당분 섭취",
-        "Cost": "COST 1",
+        "Name": "전장의 깃발",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "3턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "3칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "3칸"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Rapier/Temp.webp",
+        "Desc": "공격 범위 내 적에게 <font color=\"red\">0.36</font>배 피해<br/>명중한 적을 목표 위치 근처로 <font color=\"blue\">텔레포트</font> 및 목표 위치에 <font color=\"green\">전장의 깃발</font> 효과 부여 (지속 <font color=\"red\">1</font>턴)<br/><font color=\"green\">전장의 깃발</font> : <font color=\"blue\">[지휘]</font>주위 <font color=\"red\">2</font>칸 범위 안 적의 방어 <font color=\"red\">-10%</font><br/>지휘 범위 밖으로 이동 시 칸마다 이동력 <font color=\"red\">1</font> 추가 소모<br/> 지휘 범위 안에서 행동 종료 시 <font color=\"blue\">랜덤한 버프 2개 제거</font><br/><font color=\"orange\">정의의 결전</font> 목표는 우선적으로 목표 중심으로 <font color=\"blue\">텔레포트</font> 되며, 상술 효과를 <font color=\"red\">2</font>배로 적용 받는다<br/><font color=\"green\">전장의 깃발</font> 지형 효과는 비교적 높은 지형 우선순위를 가짐",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "기사도",
+        "Cost": "COST 2",
         "Entries": [
           {
             "Label": "유형",
@@ -273,7 +291,7 @@ var data = [
           },
           {
             "Label": "쿨타임",
-            "Value": "3턴"
+            "Value": "4턴"
           },
           {
             "Label": "사거리",
@@ -284,13 +302,13 @@ var data = [
             "Value": "단일"
           }
         ],
-        "IconImageUrl": "../img/skills/Gintoki/3.webp",
-        "Desc": "[지원] <font color =\"red\">50%</font> 생명 회복, <font color =\"blue\">당분</font>스택 <font color =\"red\">1</font>개 획득<br/>지속 <font color =\"red\">6</font>행동, <b>중첩 가능</b><br/>사용 후 <font color =\"blue\">재행동</font> <font color =\"red\">3</font>칸 <br/><font color =\"blue\">당분</font> <font color =\"red\">2</font>중첩 시 '<font color =\"blue\">당분 과다</font>' 효과 획득 <br/><font color =\"blue\">당분 과다</font> : 영웅 이동력 <font color =\"red\">-1</font>, 주는 피해량 <font color=\"red\">-15%</font>(<font color =\"red\">디메리트</font>) <br/><b>해당 스킬은 다른 버프 지속시간을 소모하지 않으며,</b><br/><font color =\"blue\">당분</font>과 <font color =\"blue\">당분 과다</font>는 <b>해제 불가,면역 불가</b><br/><font color =#83364f>백야차</font> 상태에서는 <font color =\"blue\">당분 과다</font>가 발생하지 않음",
+        "IconImageUrl": "../img/skills/Skill_KnightSoul.webp",
+        "Desc": "[지원]액티브 스킬, 자신 부대의 공격+30%, 동시에 [면역],[폭풍],[증원]효과 부여, 2턴 지속.<br/>사용 후 추가로 3칸 이동할 수 있고 공격이 가능하다.",
         "SubSkills": null,
         "Animation": null
       },
       {
-        "Name": "일섬",
+        "Name": "돌격",
         "Cost": "COST 2",
         "Entries": [
           {
@@ -299,7 +317,7 @@ var data = [
           },
           {
             "Label": "쿨타임",
-            "Value": "1턴"
+            "Value": "2턴"
           },
           {
             "Label": "사거리",
@@ -310,13 +328,13 @@ var data = [
             "Value": "단일"
           }
         ],
-        "IconImageUrl": "../img/skills/Skill_SwordCrash3.webp",
-        "Desc": "단일 개체에게 <font color=\"red\">1.4</font>배 피해<br/> 치명타율 <font color=\"Red\">+20%</font>.",
+        "IconImageUrl": "../img/skills/Skill_KnightCrash.webp",
+        "Desc": "[물리 피해]단일 적 부대를 공격해 <font color=\"#DC143C\">1배</font>의 피해를 준다.<br/>적 부대에게 '치료 방해' 부여, <font color=\"#DC143C\">1턴</font> 지속.<br/>공격 전 이동거리 1칸마다 주는 피해량 <font color=\"#DC143C\">+20%</font> (최대 <font color=\"#DC143C\">+60%</font>)",
         "SubSkills": null,
         "Animation": null
       },
       {
-        "Name": "토우야코",
+        "Name": "종전의 운검",
         "Cost": "COST 3",
         "Entries": [
           {
@@ -325,7 +343,7 @@ var data = [
           },
           {
             "Label": "쿨타임",
-            "Value": "5턴"
+            "Value": "4턴"
           },
           {
             "Label": "사거리",
@@ -337,7 +355,7 @@ var data = [
           }
         ],
         "IconImageUrl": "../img/",
-        "Desc": "[패시브] 피해를 주지 않고 행동 종료 시 '<font color=\"blue\">거합</font>'효과 획득 <br/><font color=\"blue\">거합</font> : <b>전투 진입 전</b> 주위 <font color =\"red\">1</font>바퀴 범위 내 적에게 <font color =\"red\">0.25</font>배 피해를 준다.<br/><font color=\"red\">1</font>회 효과 발동 후 <font color=\"blue\">거합</font> 효과 상실<br/>지속 <font color =\"red\">1</font>행동<br/>[물리피해]단일 개체에게 <font color =\"red\">1.8</font>배 피해.<br/>전투 진입 전 버프 <font color=\"red\">5</font>개 제거<br/> 전투 후 <font color=\"blue\">모든 장비 스킬 무효화</font> (최대 생명력 옵션 제외)<br/>지속 <font color =\"red\">2</font>행동<br/><b>해제불가</b><br/>대상이 NPC일 경우 모든 스탯 <font color =\"red\">-25%</font>로 대체 <br/>지속 <font color =\"red\">2</font>행동<br/><b>해제 불가</b><br/><b>적이 <font color=\"red\">사망</font>하지 않았을 경우 스킬 쿨타임 <font color=\"red\">-5</font> <br/>(해당 효과 쿨타임 <font color =\"red\">2</font>)",
+        "Desc": "[패시브] <font color=\"orange\">정의의 결전</font>을 보유한 적을 공격하여 전투 진입 시, 해당 전투 피해 <font color=\"red\">+25%</font><br/>[물리피해]단일 개체 <font color=\"red\">1.8</font>배 피해<br/>목표 주변 <font color=\"red\">3</font>칸 범위 안에 <font color=\"orange\">정의의 결전</font>을 보유한 적이 있을 경우,<br/>해당 적을 <font color=\"blue\">강제로 전투 진입</font> 시키고 없을 경우 해당 전투에서 <font color=\"blue\">받는 피해</font> <font color=\"red\">-25%</font><br/>해당 스킬로 적 격파 후 행동 종료 시 <font color=\"blue\">부대 생명 </font><font color=\"red\">30%</font><font color=\"blue\"> 회복 및 해당 스킬 쿨 </font><font color=\"red\">-4</font><font color=\"blue\">턴</font>, 격파한 적이 <font color=\"orange\">정의의 결전</font>을 보유하고 있었을 경우, 자신이 보유한 제거 가능한 버프 <font color=\"red\">3</font>개를 전장의 다른 '제국의 빛' 진영 아군에게 <font color=\"blue\">복사 및 고유 스킬 쿨 제거</font>",
         "SubSkills": null,
         "Animation": "../img/"
       }
