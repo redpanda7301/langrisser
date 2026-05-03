55673585fe23ec91d27376b481693662204c3129
Abysswalker-8percent <mms0208@gmail.com>
2026-05-03 20:07:30 +0900

    260503 시트 작업 (2)
    비앙카 시트 추가, 시벨리 시트 오탈자 수정

---
+++ hero/data/비앙카.js
@@ -0,0 +1,372 @@
+var data = [
+  {
+    "Skins": [
+      {
+        "ImageUrl": "비앙카/1.webp",
+        "Desc": "기본"
+      },
+      {
+        "ImageUrl": "비앙카/2.webp",
+        "Desc": "빛의 메아리"
+      }
+    ],
+    "Name": "비앙카",
+    "ChName": "贝安卡",
+    "EngName": "Bianca",
+    "JapName": "ビアンカ",
+    "CV": "타나카 아이미",
+    "Rank": [
+      0
+    ],
+    "SP": false,
+    "Groups": [
+      "제국",
+      "유성",
+      "빛군"
+    ],
+    "HP": 25,
+    "SPHP": 0,
+    "Attk": 35,
+    "SPAttk": 0,
+    "Def": 30,
+    "SPDef": 0,
+    "MagicDef": 10,
+    "SPMagicDef": 0,
+    "HowToGet": "영웅 소환",
+    "Fetters": [
+      {
+        "Condition": "레벨 30달성"
+      },
+      {
+        "Condition": "비앙카를 출전시켜 시공의 균열 3-1[정예] 클리어"
+      },
+      {
+        "Condition": "시벨리의 도움을 받아 운명의 문4 클리어"
+      },
+      {
+        "Condition": "비앙카를 화염 선봉 대장으로 전직"
+      }
+    ],
+    "HeartFetter": "생명+900, 방어+60, 마방+30",
+    "Talent": [
+      {
+        "TalentName": "용화의 수칙",
+        "Icon": "Gift_Bianca.webp",
+        "Abilities": [
+          {
+            "Desc": "1성"
+          },
+          {
+            "Desc": "2성"
+          },
+          {
+            "Desc": "공격, 방어 <font color=\"red\">+10%</font><br/>행동 종료 시. 추가로 하기의 <font color=\"blue\">창순술</font> 사용 가능<br/>1) <font color=\"blue\">용암 방패</font> : 액티브, <font color=\"blue\">쉴드</font> 와 <font color=\"blue\">지휘</font>를 획득<br/><font color=\"blue\">쉴드</font> : 최대 생명 <font color=\"red\">25%</font> 수치의 피해를 막아주며, <font color=\"blue\">피격 시 치명타가 발생하지 않음</font><br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">2</font>칸 범위 안 적은 <font color=\"blue\">재행동 불가</font><br/>2) <font color=\"blue\">불꽃의 창 폭발</font> : 액티브, <font color=\"blue\">용의 불꽃</font> 획득<br/><font color=\"blue\">용의 불꽃</font> : <font color=\"blue\">주는 피해량</font> <font color=\"red\">+20%</font>, 선공으로 전투 진입 전에 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">창순술</font>의 효과는 모두 <font color=\"red\">2</font>턴 지속, <font color=\"blue\">해제 불가</font>, 쿨타임 <font color=\"red\">4</font>턴<br/><br/><i>(<font color=\"blue\">용의 불꽃</font>으로 발생하는 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 는 해제 가능)</i>"
+          },
+          {
+            "Desc": "공격, 방어 <font color=\"red\">+15%</font><br/>행동 종료 시. 추가로 하기의 <font color=\"blue\">창순술</font> 사용 가능<br/>1) <font color=\"blue\">용암 방패</font> : 액티브, <font color=\"blue\">쉴드</font> 와 <font color=\"blue\">지휘</font>를 획득<br/><font color=\"blue\">쉴드</font> : 최대 생명 <font color=\"red\">25%</font> 수치의 피해를 막아주며, <font color=\"blue\">피격 시 치명타가 발생하지 않음</font><br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">2</font>칸 범위 안 적은 <font color=\"blue\">재행동 불가</font><br/>2) <font color=\"blue\">불꽃의 창 폭발</font> : 액티브, <font color=\"blue\">용의 불꽃</font> 획득<br/><font color=\"blue\">용의 불꽃</font> : <font color=\"blue\">주는 피해량</font> <font color=\"red\">+20%</font>, 선공으로 전투 진입 전에 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">창순술</font>의 효과는 모두 <font color=\"red\">2</font>턴 지속, <font color=\"blue\">해제 불가</font>, 쿨타임 <font color=\"red\">3</font>턴<br/><br/><i>(<font color=\"blue\">용의 불꽃</font>으로 발생하는 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 는 해제 가능)</i>"
+          },
+          {
+            "Desc": "공격, 방어 <font color=\"red\">+20%</font><br/>행동 종료 시. 추가로 하기의 <font color=\"blue\">창순술</font> 사용 가능<br/>1) <font color=\"blue\">용암 방패</font> : 액티브, <font color=\"blue\">쉴드</font> 와 <font color=\"blue\">지휘</font>를 획득<br/><font color=\"blue\">쉴드</font> : 최대 생명 <font color=\"red\">25%</font> 수치의 피해를 막아주며, <font color=\"blue\">피격 시 치명타가 발생하지 않음</font><br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">2</font>칸 범위 안 적은 <font color=\"blue\">재행동 불가</font><br/>2) <font color=\"blue\">불꽃의 창 폭발</font> : 액티브, <font color=\"blue\">용의 불꽃</font> 획득<br/><font color=\"blue\">용의 불꽃</font> : <font color=\"blue\">주는 피해량</font> <font color=\"red\">+20%</font>, 선공으로 전투 진입 전에 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">창순술</font>의 효과는 모두 <font color=\"red\">2</font>턴 지속, <font color=\"blue\">해제 불가</font>, 쿨타임 <font color=\"red\">2</font>턴<br/><br/><i>(<font color=\"blue\">용의 불꽃</font>으로 발생하는 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 는 해제 가능)</i>"
+          },
+          {
+            "Desc": "공격, 방어 <font color=\"red\">+25%</font><br/>행동 종료 시. 추가로 하기의 <font color=\"blue\">창순술</font> 사용 가능<br/>1) <font color=\"blue\">용암 방패</font> : 액티브, <font color=\"blue\">쉴드</font> 와 <font color=\"blue\">지휘</font>를 획득<br/><font color=\"blue\">쉴드</font> : 최대 생명 <font color=\"red\">25%</font> 수치의 피해를 막아주며, <font color=\"blue\">피격 시 치명타가 발생하지 않음</font><br/><font color=\"blue\">지휘</font> : 주위 <font color=\"red\">2</font>칸 범위 안 적은 <font color=\"blue\">재행동 불가</font><br/>2) <font color=\"blue\">불꽃의 창 폭발</font> : 액티브, <font color=\"blue\">용의 불꽃</font> 획득<br/><font color=\"blue\">용의 불꽃</font> : <font color=\"blue\">주는 피해량</font> <font color=\"red\">+20%</font>, 선공으로 전투 진입 전에 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">창순술</font>의 효과는 모두 <font color=\"red\">2</font>턴 지속, <font color=\"blue\">해제 불가</font>, 쿨타임 <font color=\"red\">2</font>턴<br/><br/><i>(<font color=\"blue\">용의 불꽃</font>으로 발생하는 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 는 해제 가능)</i>"
+          }
+        ]
+      }
+    ],
+    "SPTalent": null,
+    "Second_Awaken": null,
+    "Equip": null,
+    "Soldiers": [
+      "중장 창병",
+      "그레나디어",
+      "중장 기병",
+      "로열 그리폰",
+      "암흑 근위대",
+      "송곳니 근위대",
+      "불꽃 카발리에",
+      "기계 용기사",
+      "암흑 수정 용기병",
+      "",
+      ""
+    ],
+    "Jobs": [
+      {
+        "Name": "용화의 장군",
+        "Icon": "창병",
+        "SD": [
+          {
+            "ImageUrl": "비앙카/1/1.webp"
+          },
+          {
+            "ImageUrl": "비앙카/2/1.webp"
+          }
+        ],
+        "AttkRange": 1,
+        "Move": 3,
+        "MoveType": "보행",
+        "HP": 5207,
+        "HPRate": 80,
+        "Attk": 581,
+        "AttkRate": 89,
+        "Int": 262,
+        "IntRate": 40,
+        "Def": 339,
+        "DefRate": 75,
+        "MagicDef": 265,
+        "MagicDefRate": 58,
+        "Skill": 111,
+        "SkillRate": 31,
+        "HeartFetterBouns1": "디버프를 보유한 부대에게 피격으로 전투 진입 시<br/>받는 피해량 <font color=\"Red\">-10%</font>",
+        "HeartFetterBouns2": "근접 공격을 받아 전투 진입 시<br/>주는 피해량 <font color=\"red\">+10%</font>"
+      },
+      {
+        "Name": "화염 선봉 대장",
+        "Icon": "기병",
+        "SD": [
+          {
+            "ImageUrl": "비앙카/1/2.webp"
+          },
+          {
+            "ImageUrl": "비앙카/2/2.webp"
+          }
+        ],
+        "AttkRange": 1,
+        "Move": 5,
+        "MoveType": "기마",
+        "HP": 4879,
+        "HPRate": 75,
+        "Attk": 581,
+        "AttkRate": 89,
+        "Int": 239,
+        "IntRate": 36,
+        "Def": 315,
+        "DefRate": 70,
+        "MagicDef": 265,
+        "MagicDefRate": 58,
+        "Skill": 125,
+        "SkillRate": 35,
+        "HeartFetterBouns1": "부대 생명 <font color=\"red\">70%</font> 미만일 시,<br/>받는 피해량 <font color=\"red\">-10%</font>",
+        "HeartFetterBouns2": "자신 부대에 버프가 <font color=\"red\">5</font>개 이상 부여되어 있을 시,<br/>전투 중 주는 피해량 <font color=\"red\">+10%</font>"
+      }
+    ],
+    "SPOpenConditions": null,
+    "Skills": [
+      {
+        "Name": "관통",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/43.webp",
+        "Desc": "[물리 피해] 단일 적 부대를 공격해 1.5배의 전투 피해를 준다.<br/>전투 후, 적 방어 -30%, 2턴 지속.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "지원 방어",
+        "Cost": "COST 1",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "액티브"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "4턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "3칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "1칸"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Bianca/Skill_Bianca_3.webp",
+        "Desc": "<font color=\"red\">3</font>칸 범위 안 <font color=\"green\">방어 지형</font> 혹은 <font color=\"blue\">아군</font>에게 사용<br/>목표와 가장 가까운 지점으로 <font color=\"blue\">텔레포트</font> 및 <font color=\"blue\">면역</font>과 <font color=\"blue\">증원</font> 효과 획득 (지속 <font color=\"red\">2</font>턴)<br/>이후 <font color=\"red\">3</font>칸 <font color=\"blue\">재행동</font><br/>해당 재행동은 <font color=\"blue\">보유한 버프의 지속 시간을 소모하지 않음</font><br/><br/><font color=\"blue\">면역</font> : 모든 디버프 면역<br/><font color=\"blue\">증원</font> : 행동 종료 시, 자신 부대 생명 <font color=\"red\">20%</font> 회복",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "배수의 진",
+        "Cost": "COST 1",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "패시브"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Passive_DefBuf3.webp",
+        "Desc": "부대 생명이 <font color=\"Red\">50%</font> 이하로 전투 진입 시 방어, 마방 <font color=\"Red\">+10%</font>.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "정면일격",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "5턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_Support.webp",
+        "Desc": "[물리 피해] 단일 적군을 공격하여 1.7배 피해를 준다.<br/>전투 전 공격의 20%가 방어에 추가되며, 적 부대에게 '모든 패시브 스킬을 무효화한다'부여,2턴 지속,<br/>이 효과는 제거되지 않는다.",
+        "SubSkills": null,
+        "Animation": null
+      },
+     {
+        "Name": "바위 방패의 수호",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "액티브"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "자신"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Bianca/Skill_Bianca_4.webp",
+        "Desc": "[패시브] 인접 아군이 받는 모든 공격 <font color=\"blue\">호위</font><br/>주위 <font color=\"red\">2</font>칸 범위 안에 아군이 있을 경우, 사망 시 생명 <font color=\"red\">20%</font> 수치로 <font color=\"blue\">부활</font> (<font color=\"blue\">부활</font> 효과 쿨타임 <font color=\"red\">5</font>턴)<br/>[액티브] <font color=\"blue\">호위</font> 범위가 <font color=\"red\">2</font>칸으로 확장 및 <font color=\"blue\">바위 호위</font> 효과 획득<br/><font color=\"blue\">바위 호위</font> : 피격으로 전투 진입 시 <font color=\"blue\">2칸 원거리 공격 반격 가능</font> 및 전투 후 <font color=\"blue\">쉴드</font> 를 획득. <font color=\"blue\">호위</font> 발동 시 받는 피해량 <font color=\"red\">-20%</font> (지속 <font color=\"red\">3</font>턴)<br/><br/><font color=\"blue\">쉴드</font> : 최대 생명 <font color=\"red\">25%</font> 수치의 피해를 막아주며, <font color=\"blue\">피격 시 치명타가 발생하지 않음</font>",
+        "SubSkills": null,
+        "Animation": null
+      },
+       {
+        "Name": "불꽃의 창 돌격",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "2턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_Support.webp",
+        "Desc": "[패시브] 전장의 임의 부대가 사망한 후, 자신은 <font color=\"blue\">용의 불꽃</font> 및 <font color=\"blue\">야전</font> 효과 획득 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">용의 불꽃</font> : <font color=\"blue\">주는 피해량</font> <font color=\"red\">+20%</font>, 선공으로 전투 진입 전에 <font color=\"blue\">적의 방어구 효과 무효화 (최대 생명 제외)</font> 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">받는 피해량</font> <font color=\"red\">+25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴)<br/><font color=\"blue\">야전</font>: 이동하는 모든 지형을 평지로 간주<br/>[물리피해] 단일 개체 <font color=\"red\">1.6</font>배 피해 및 <font color=\"blue\">다음에 스킬 피격으로 전투 진입 시 적의 해당 스킬 쿨 -<font color=\"red\">3</font>턴</font> 효과 부여<br/>전투 전에 목표에 인접한 다른 적이 없을 경우, 전투 후 <font color=\"blue\">재이동</font> <font color=\"red\">3</font>칸<br/><font color=\"blue\">용의 불꽃</font> 보유 시 <font color=\"blue\">사거리</font> <font color=\"red\">+1</font> 및 <font color=\"blue\">근접전 병사 대동 시 함께 공격</font>",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "군단 정비",
+        "Cost": "COST 1",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "패시브"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Passive_BothBuf1.webp",
+        "Desc": "[패시브]부대 생명이 90% 이상일 시 공격,방어 +10%.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "진형파괴",
+        "Cost": "COST 2",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "3턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "1칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/skills/Skill_KnightCrash3.webp",
+        "Desc": "[물리 피해]단일 적 부대를 공격해 1배의 공격 피해를 준다.<br/>대상을 2칸 밀쳐내며 다음 턴 행동 불가 부여.",
+        "SubSkills": null,
+        "Animation": null
+      },
+      {
+        "Name": "대지를 태우는 용의 숨결포",
+        "Cost": "COST 3",
+        "Entries": [
+          {
+            "Label": "유형",
+            "Value": "물리피해"
+          },
+          {
+            "Label": "쿨타임",
+            "Value": "5턴"
+          },
+          {
+            "Label": "사거리",
+            "Value": "2칸"
+          },
+          {
+            "Label": "범위",
+            "Value": "단일"
+          }
+        ],
+        "IconImageUrl": "../img/Super/비앙카.webp",
+        "Desc": "단일 개체 <font color=\"red\">1.7</font>배 피해<br/>전투 전 적의 버프 <font color=\"red\">3</font>개 제거<br/><font color=\"blue\">근접전 병사 대동 시 병사도 함께 공격</font><br/><font color=\"blue\">해당 스킬은 근접전 패널티를 받지 않음</font><br/><font color=\"blue\">용의 불꽃</font> 보유 시, 전투 후 목표 주변 <font color=\"red\">3</font>칸 범위 안 모든 적에게 <font color=\"red\">0.3</font>배 범위 피해 및 <font color=\"blue\">무기 효과 무효화 (최대 생명 제외)</font> 효과 부여<br/>목표가 NPC일 경우 해당 효과는 <font color=\"blue\">주는 피해량</font> <font color=\"red\">-25%</font> 로 대체 (지속 <font color=\"red\">2</font>턴, <font color=\"blue\">해제 불가</font>)<br/><font color=\"blue\">쉴드</font> 보유 시, 해당 스킬에 피격당한 적은 <font color=\"blue\">이동 및 호위 불가</font> (지속 <font color=\"red\">1</font>턴)",
+        "SubSkills": null,
+        "Animation": "../img/Super/비앙카_Ani.webp"
+      }
+    ],
+    "Propose": false,
+    "Bio": null,
+    "Performance": null,
+    "Performance2": null,
+    "Misc": ""
+  }
+]
